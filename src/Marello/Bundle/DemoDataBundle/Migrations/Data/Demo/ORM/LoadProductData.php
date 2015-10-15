d21df163e81c3c02bdc9c64efefdfbef57383f27
Hotlander <hotlander@gmail.com>
2015-10-15 15:13:53 +0200

    - Fixed bugs and errors on install - Added Product demo data

---
+++ src/Marello/Bundle/DemoDataBundle/Migrations/Data/Demo/ORM/LoadProductData.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Marello\Bundle\DemoDataBundle\Migrations\Data\Demo\ORM;
+
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+use Marello\Bundle\ProductBundle\Entity\Product;
+use Marello\Bundle\ProductBundle\Entity\ProductStatus;
+
+class LoadProductData extends AbstractFixture implements DependentFixtureInterface {
+
+    public function getDependencies() {
+        return ['Marello\Bundle\DemoDataBundle\Migrations\Data\Demo\ORM\LoadProductStatusData'];
+    }
+
+    /**
+     * {@inheritDoc}
+     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
+     */
+    public function load(ObjectManager $manager)
+    {
+        $this->loadProducts($manager);
+        $manager->flush();
+    }
+
+    /**
+     *
+     */
+    public function loadProducts($manager)
+    {
+        $handle = fopen(__DIR__ . DIRECTORY_SEPARATOR . 'dictionaries' . DIRECTORY_SEPARATOR . "products.csv", "r");
+        if ($handle) {
+            $headers = array();
+            $products = array();
+            if (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                //read headers
+                $headers = $data;
+            }
+            $i = 0;
+            while (($data = fgetcsv($handle, 1000, ",")) !== false) {
+                $data = array_combine($headers, array_values($data));
+
+                $this->createProduct($data, $manager);
+                $i++;
+            }
+            fclose($handle);
+        }
+    }
+
+    private function createProduct(array $data, $manager)
+    {
+        $product = new Product();
+        $product->setSku($data['sku']);
+        $product->setPrice($data['price']);
+        $product->setName($data['name']);
+        $product->setStockLevel($data['stock_level']);
+        $manager->persist($product);
+    }
+}
