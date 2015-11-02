78cfa437bd3ef1e66a8d5eff76fc95da496298ae
jakubbabiak <jakub@babiak.cz>
2015-11-02 14:50:58 +0100

    Enterprise Inventory Warehouse management  - Warehouse CRUD  - Option to set default warehouse  - Event listener to force only one default warehouse

---
+++ Bundle/InventoryBundle/Form/Extension/WarehouseExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace MarelloEnterprise\Bundle\InventoryBundle\Form\Extension;
+
+use Marello\Bundle\InventoryBundle\Form\Type\WarehouseType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class WarehouseExtension extends AbstractTypeExtension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getExtendedType()
+    {
+        return WarehouseType::NAME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+
+        $builder->add('default', 'checkbox', [
+            'required' => false
+        ]);
+    }
+
+}
