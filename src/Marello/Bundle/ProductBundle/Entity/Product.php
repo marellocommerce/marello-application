4627e83655e301c6825f57ac6282ff88c076e490
Hotlander <hotlander@gmail.com>
2015-11-26 10:08:08 +0100

    - Small fixes for saving variants

--- src/Marello/Bundle/ProductBundle/Entity/Product.php
+++ src/Marello/Bundle/ProductBundle/Entity/Product.php
@@ -2,19 +2,20 @@
 
 namespace Marello\Bundle\ProductBundle\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 
-use Oro\Bundle\OrganizationBundle\Entity\Organization;
 use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\Config;
 use Oro\Bundle\EntityConfigBundle\Metadata\Annotation\ConfigField;
+use Oro\Bundle\OrganizationBundle\Entity\Organization;
 
 use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
 use Marello\Bundle\PricingBundle\Entity\ProductPrice;
+use Marello\Bundle\ProductBundle\Model\ExtendProduct;
 use Marello\Bundle\SalesBundle\Entity\SalesChannel;
 use Marello\Bundle\SalesBundle\Model\SalesChannelAwareInterface;
 use Marello\Bundle\PricingBundle\Model\PricingAwareInterface;
-use Marello\Bundle\ProductBundle\Model\ExtendProduct;
 
 /**
  * Represents a Marello Product
@@ -127,7 +128,12 @@ class Product extends ExtendProduct implements
     /**
      * @var Collection|ProductPrice[]
      *
-     * @ORM\OneToMany(targetEntity="Marello\Bundle\PricingBundle\Entity\ProductPrice", mappedBy="product", cascade={"persist"}, orphanRemoval=true)
+     * @ORM\OneToMany(
+     *     targetEntity="Marello\Bundle\PricingBundle\Entity\ProductPrice",
+     *     mappedBy="product",
+     *     cascade={"persist"},
+     *     orphanRemoval=true
+     * )
      * @ORM\OrderBy({"id" = "ASC"})
      */
     protected $prices;
@@ -142,7 +148,7 @@ class Product extends ExtendProduct implements
     /**
      * @var Variant
      *
-     * @ORM\ManyToOne(targetEntity="Variant")
+     * @ORM\ManyToOne(targetEntity="Variant", inversedBy="products")
      * @ORM\JoinColumn(name="variant_id", referencedColumnName="id", onDelete="SET NULL")
      */
     protected $variant;
@@ -221,6 +227,7 @@ class Product extends ExtendProduct implements
 
     /**
      * @param string $name
+     *
      * @return Product
      */
     public function setName($name)
@@ -240,6 +247,7 @@ class Product extends ExtendProduct implements
 
     /**
      * @param string $sku
+     *
      * @return Product
      */
     public function setSku($sku)
@@ -259,6 +267,7 @@ class Product extends ExtendProduct implements
 
     /**
      * @param float $price
+     *
      * @return Product
      */
     public function setPrice($price)
@@ -278,6 +287,7 @@ class Product extends ExtendProduct implements
 
     /**
      * @param ProductStatus $status
+     *
      * @return Product
      */
     public function setStatus(ProductStatus $status)
@@ -299,6 +309,7 @@ class Product extends ExtendProduct implements
      * Add item
      *
      * @param ProductPrice $price
+     *
      * @return Product
      */
     public function addPrice(ProductPrice $price)
@@ -315,6 +326,7 @@ class Product extends ExtendProduct implements
      * Remove item
      *
      * @param ProductPrice $price
+     *
      * @return Product
      */
     public function removePrice(ProductPrice $price)
@@ -344,6 +356,7 @@ class Product extends ExtendProduct implements
 
     /**
      * @param Variant $variant
+     *
      * @return Product
      */
     public function setVariant(Variant $variant)
@@ -357,6 +370,7 @@ class Product extends ExtendProduct implements
      * Add item
      *
      * @param SalesChannel $channel
+     *
      * @return Product
      */
     public function addChannel(SalesChannel $channel)
@@ -372,6 +386,7 @@ class Product extends ExtendProduct implements
      * Remove item
      *
      * @param SalesChannel $channel
+     *
      * @return Product
      */
     public function removeChannel(SalesChannel $channel)
@@ -379,6 +394,25 @@ class Product extends ExtendProduct implements
         if ($this->channels->contains($channel)) {
             $this->channels->removeElement($channel);
         }
+
+        return $this;
+    }
+
+    /**
+     * @return Organization
+     */
+    public function getOrganization()
+    {
+        return $this->organization;
+    }
+
+    /**
+     * @param Organization $organization
+     * @return Product
+     */
+    public function setOrganization($organization)
+    {
+        $this->organization = $organization;
 
         return $this;
     }
@@ -403,25 +437,6 @@ class Product extends ExtendProduct implements
     }
 
     /**
-     * @return Organization
-     */
-    public function getOrganization()
-    {
-        return $this->organization;
-    }
-
-    /**
-     * @param Organization $organization
-     * @return Product
-     */
-    public function setOrganization($organization)
-    {
-        $this->organization = $organization;
-
-        return $this;
-    }
-
-    /**
      * @return \DateTime
      */
     public function getCreatedAt()
@@ -431,6 +446,7 @@ class Product extends ExtendProduct implements
 
     /**
      * @param \DateTime $createdAt
+     *
      * @return Product
      */
     public function setCreatedAt($createdAt)
@@ -450,6 +466,7 @@ class Product extends ExtendProduct implements
 
     /**
      * @param \DateTime $updatedAt
+     *
      * @return Product
      */
     public function setUpdatedAt($updatedAt)
@@ -492,15 +509,27 @@ class Product extends ExtendProduct implements
     }
 
     /**
-     * @param Collection|InventoryItem[] $inventoryItems
+     * @param InventoryItem $item
      *
      * @return $this
      */
-    public function setInventoryItems($inventoryItems)
+    public function addInventoryItem(InventoryItem $item)
     {
-        $this->inventoryItems = $inventoryItems;
+        $item->setProduct($this);
+        $this->inventoryItems->add($item);
 
         return $this;
     }
 
+    /**
+     * @param InventoryItem $item
+     *
+     * @return $this
+     */
+    public function removeInventoryItem(InventoryItem $item)
+    {
+        $this->inventoryItems->removeElement($item);
+
+        return $this;
+    }
 }
