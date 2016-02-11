8b3573da3140ff61da55b3f9c88c10376b0d0036
jakubbabiak <jakub@babiak.cz>
2016-02-11 16:11:58 +0100

    - Forgotten import

--- src/Marello/Bundle/InventoryBundle/DependencyInjection/MarelloInventoryExtension.php
+++ src/Marello/Bundle/InventoryBundle/DependencyInjection/MarelloInventoryExtension.php
@@ -2,7 +2,6 @@
 
 namespace Marello\Bundle\InventoryBundle\DependencyInjection;
 
-use Marello\Bundle\InventoryBundle\DependencyInjection\Compiler\InventoryLogActionHandlerCompilerPass;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
