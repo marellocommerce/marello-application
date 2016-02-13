4367933dcce7182b8d280c7d32ca9b7a4d8e94aa
Hotlander <hotlander@gmail.com>
2016-02-13 11:35:35 +0100

    - Added translation - Updated header for export inventory export template - Changed replace counter for update counter

--- src/Marello/Bundle/InventoryBundle/Entity/InventoryItem.php
+++ src/Marello/Bundle/InventoryBundle/Entity/InventoryItem.php
@@ -52,7 +52,6 @@ class InventoryItem
      *      defaultValues={
      *          "importexport"={
      *              "order"=10,
-     *              "header"="",
      *              "full"=true,
      *          }
      *      }
