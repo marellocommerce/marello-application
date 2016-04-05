d23a1f58b5fad9c2304b634fad830a13b100f92b
jakubbabiak <jakub@babiak.cz>
2016-04-05 17:12:29 +0200

    Added view method test for PO controller.

--- src/Marello/Bundle/PurchaseOrderBundle/Tests/Functional/Controller/PurchaseOrderControllerTest.php
+++ src/Marello/Bundle/PurchaseOrderBundle/Tests/Functional/Controller/PurchaseOrderControllerTest.php
@@ -2,14 +2,20 @@
 
 namespace Marello\Bundle\PurchaseOrderBundle\Tests\Functional\Controller;
 
+use Marello\Bundle\DemoDataBundle\Migrations\Data\Demo\ORM\LoadPurchaseOrderData;
+use Marello\Bundle\PurchaseOrderBundle\Entity\PurchaseOrder;
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
+/**
+ * @dbIsolation
+ */
 class PurchaseOrderControllerTest extends WebTestCase
 {
     protected function setUp()
     {
         $this->initClient($this->generateBasicAuthHeader());
+        $this->loadFixtures([LoadPurchaseOrderData::class]);
     }
 
     /** @test */
@@ -20,8 +26,17 @@ class PurchaseOrderControllerTest extends WebTestCase
         $this->assertHtmlResponseStatusCodeEquals($this->client->getResponse(), Response::HTTP_OK);
     }
 
+    /** @test */
     public function testViewAction()
     {
-        
+        /** @var PurchaseOrder $po */
+        $po = $this->getReference('marello-purchase-order-1');
+
+        $this->client->request(
+            'GET',
+            $this->getUrl('marello_purchaseorder_purchaseorder_view', ['id' => $po->getId()])
+        );
+
+        $this->assertHtmlResponseStatusCodeEquals($this->client->getResponse(), Response::HTTP_OK);
     }
 }
