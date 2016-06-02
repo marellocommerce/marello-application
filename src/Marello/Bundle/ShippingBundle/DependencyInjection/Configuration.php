14af87ceaf08accc0f6113a10a206d9bad3fd5a4
jakubbabiak <jakub@babiak.cz>
2016-06-02 17:03:02 +0200

    Additional shipper configuration.

--- src/Marello/Bundle/ShippingBundle/DependencyInjection/Configuration.php
+++ src/Marello/Bundle/ShippingBundle/DependencyInjection/Configuration.php
@@ -22,9 +22,22 @@ class Configuration implements ConfigurationInterface
         SettingsBuilder::append(
             $rootNode,
             [
-                'ups_username'           => ['value' => null],
-                'ups_password'           => ['value' => null],
-                'ups_access_license_key' => ['value' => null],
+                'shipper_name'                 => ['value' => null],
+                'shipper_attention_name'       => ['value' => null],
+                'shipper_tax_id'               => ['value' => null],
+                'shipper_phone'                => ['value' => null],
+                'shipper_email'                => ['value' => null],
+                'shipper_address_line_1'       => ['value' => null],
+                'shipper_address_line_2'       => ['value' => null],
+                'shipper_address_line_3'       => ['value' => null],
+                'shipper_address_city'         => ['value' => null],
+                'shipper_address_state'        => ['value' => null],
+                'shipper_address_postal_code'  => ['value' => null],
+                'shipper_address_country_code' => ['value' => null],
+                'ups_username'                 => ['value' => null],
+                'ups_password'                 => ['value' => null],
+                'ups_access_license_key'       => ['value' => null],
+                'ups_account_number'           => ['value' => null],
             ]
         );
 
