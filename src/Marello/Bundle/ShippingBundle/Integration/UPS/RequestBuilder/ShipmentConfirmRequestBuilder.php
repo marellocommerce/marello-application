869071c9d05488ce7a178556b0949495b0f3fcd4
jakubbabiak <jakub@babiak.cz>
2016-06-03 16:29:57 +0200

    Added Shipment building to shipment confirm request builder.

--- src/Marello/Bundle/ShippingBundle/Integration/UPS/RequestBuilder/ShipmentConfirmRequestBuilder.php
+++ src/Marello/Bundle/ShippingBundle/Integration/UPS/RequestBuilder/ShipmentConfirmRequestBuilder.php
@@ -4,6 +4,7 @@ namespace Marello\Bundle\ShippingBundle\Integration\UPS\RequestBuilder;
 
 use DOMDocument;
 use DOMElement;
+use Marello\Bundle\ShippingBundle\Integration\UPS\Model\Shipment;
 
 class ShipmentConfirmRequestBuilder extends RequestBuilder
 {
@@ -20,6 +21,9 @@ class ShipmentConfirmRequestBuilder extends RequestBuilder
         $xml->appendChild($root = $xml->createElement('ShipmentConfirmRequest'));
 
         $this->buildRequest($xml, $root);
+        $this->buildShipment($data, $xml, $root);
+
+        return $xml->saveXML();
     }
 
     protected function buildRequest(DOMDocument $xml, DOMElement $parent)
@@ -29,8 +33,14 @@ class ShipmentConfirmRequestBuilder extends RequestBuilder
         $request->appendChild($transaction = $xml->importNode($this->createTransactionNode(), true));
 
         $request->appendChild($xml->createElement('RequestAction', 'ShipConfirm'));
-        $request->appendChild($xml->createElement('RequestOption', 'nonvalidate'));
-        
-        
+        $request->appendChild($xml->createElement('RequestOption', 'validate'));
+    }
+
+    protected function buildShipment(array $data, DOMDocument $xml, DOMElement $parent)
+    {
+        /** @var Shipment $shipment */
+        $shipment = $data['shipment'];
+
+        $shipment->toXmlNode($xml, $parent);
     }
 }
