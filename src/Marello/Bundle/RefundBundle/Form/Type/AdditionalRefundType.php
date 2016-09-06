829db004f601dcf91770827a519e5f7dcde277b3
Jakub Babiak <jakub@babiak.cz>
2016-09-06 17:23:09 +0200

    Display currencies in refund forms.

--- src/Marello/Bundle/RefundBundle/Form/Type/AdditionalRefundType.php
+++ src/Marello/Bundle/RefundBundle/Form/Type/AdditionalRefundType.php
@@ -2,9 +2,12 @@
 
 namespace Marello\Bundle\RefundBundle\Form\Type;
 
+use Marello\Bundle\RefundBundle\Entity\Refund;
 use Marello\Bundle\RefundBundle\Entity\RefundItem;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\GreaterThan;
 use Symfony\Component\Validator\Constraints\NotNull;
@@ -21,13 +24,34 @@ class AdditionalRefundType extends AbstractType
                 'constraints' => [
                     new NotNull(),
                 ]
-            ])
-            ->add('refundAmount', 'oro_money', [
-                'empty_data' => 0,
-                'constraints' => [
-                    new GreaterThan(0),
-                ]
             ]);
+
+        $builder
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                /** @var RefundItem $item */
+                $item = $event->getData();
+                $form = $event->getForm();
+
+                if ($item === null) {
+                    $form->add('refundAmount', 'money', [
+                        'empty_data' => 0,
+                        'constraints' => [
+                            new GreaterThan(0),
+                        ]
+                    ]);
+
+                    return;
+                }
+
+                $form
+                    ->add('refundAmount', 'money', [
+                        'empty_data' => 0,
+                        'currency' => $item->getRefund()->getCurrency(),
+                        'constraints' => [
+                            new GreaterThan(0),
+                        ]
+                    ]);
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Marello/Bundle/RefundBundle/Form/Type/OrderItemRefundType.php
+++ src/Marello/Bundle/RefundBundle/Form/Type/OrderItemRefundType.php
@@ -5,7 +5,10 @@ namespace Marello\Bundle\RefundBundle\Form\Type;
 use Marello\Bundle\RefundBundle\Entity\RefundItem;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThan;
 
 class OrderItemRefundType extends AbstractType
 {
@@ -14,8 +17,29 @@ class OrderItemRefundType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('quantity')
-            ->add('refundAmount', 'oro_money');
+            ->add('quantity');
+
+        $builder
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+                /** @var RefundItem $item */
+                $item = $event->getData();
+                $form = $event->getForm();
+
+                if ($item === null) {
+                    $form->add('refundAmount', 'money');
+
+                    return;
+                }
+
+                $form
+                    ->add('refundAmount', 'money', [
+                        'empty_data' => 0,
+                        'currency' => $item->getRefund()->getCurrency(),
+                        'constraints' => [
+                            new GreaterThan(0),
+                        ]
+                    ]);
+            });
     }
 
     public function configureOptions(OptionsResolver $resolver)
