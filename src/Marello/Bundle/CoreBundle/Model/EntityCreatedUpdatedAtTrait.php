56e0e30a62eec5a42a35befe6714dde9ddbdd71e
Hotlander <j.casteleijn@labelgoods.nl>
2016-10-05 13:12:06 +0200

    - added created at / updated at trait for entities

---
+++ src/Marello/Bundle/CoreBundle/Model/EntityCreatedUpdatedAtTrait.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Marello\Bundle\CoreBundle\Model;
+
+trait EntityCreatedUpdatedAtTrait
+{
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     * @Oro\ConfigField(
+     *      defaultValues={
+     *          "entity"={
+     *              "label"="oro.ui.created_at"
+     *          }
+     *      }
+     * )
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(type="datetime")
+     * @Oro\ConfigField(
+     *      defaultValues={
+     *          "entity"={
+     *              "label"="oro.ui.updated_at"
+     *          }
+     *      }
+     * )
+     */
+    protected $updatedAt;
+
+
+    /**
+     * @return \Datetime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return \Datetime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function preUpdate()
+    {
+        $this->updatedAt = new \DateTime('now', new \DateTimeZone('UTC'));
+    }
+
+    /**
+     * @ORM\PrePersist
+     */
+    public function prePersist()
+    {
+        $this->createdAt = $this->updatedAt = new \DateTime();
+    }
+}
