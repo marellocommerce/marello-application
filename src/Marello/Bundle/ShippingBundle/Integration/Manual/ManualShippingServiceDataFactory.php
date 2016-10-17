6e5de2d98956efa18460b70d5f3f5853af257840
Joey Masip Romeu <joey@madia.nl>
2016-10-17 11:02:58 +0200

    manual shiping - no need configurating

---
+++ src/Marello/Bundle/ShippingBundle/Integration/Manual/ManualShippingServiceDataFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Marello\Bundle\ShippingBundle\Integration\Manual;
+
+use Doctrine\Bundle\DoctrineBundle\Registry;
+
+use Marello\Bundle\ShippingBundle\Integration\ShippingServiceDataProviderInterface;
+use Marello\Bundle\ShippingBundle\Integration\ShippingServiceDataFactoryInterface;
+use Oro\Bundle\ConfigBundle\Config\ConfigManager;
+
+class ManualShippingServiceDataFactory implements ShippingServiceDataFactoryInterface
+{
+    /** @var ConfigManager */
+    protected $configManager;
+
+    /**
+     * ManualShippingServiceDataFactory constructor.
+     *
+     * @param ConfigManager $configManager
+     */
+    public function __construct(ConfigManager $configManager)
+    {
+        $this->configManager = $configManager;
+    }
+
+    /**
+     * @param ShippingServiceDataProviderInterface $shippingDataProvider
+     * @return array
+     */
+    public function createData(ShippingServiceDataProviderInterface $shippingDataProvider)
+    {
+        return [];
+    }
+
+}
