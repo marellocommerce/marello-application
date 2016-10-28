d55c1cff50922de68e952f2b471347324ea2c21d
Joey Masip Romeu <joey@madia.nl>
2016-10-28 16:57:13 +0200

    update strategy method signature

--- src/Marello/Bundle/InventoryBundle/ImportExport/Strategy/InventoryItemUpdateStrategy.php
+++ src/Marello/Bundle/InventoryBundle/ImportExport/Strategy/InventoryItemUpdateStrategy.php
@@ -14,6 +14,7 @@ class InventoryItemUpdateStrategy extends ConfigurableAddOrReplaceStrategy
      * @param bool                 $isPersistNew
      * @param null                 $itemData
      * @param array                $searchContext
+     * @param bool                 $entityIsRelation
      *
      * @return null
      */
@@ -22,7 +23,8 @@ class InventoryItemUpdateStrategy extends ConfigurableAddOrReplaceStrategy
         $isFullData = false,
         $isPersistNew = false,
         $itemData = null,
-        array $searchContext = []
+        array $searchContext = [],
+        $entityIsRelation = false
     ) {
         $item = $this->findInventoryItem($entity);
 
