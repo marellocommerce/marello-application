08f4326b009bcb907f379730d2ac4d416c6c27fc
Hotlander <j.casteleijn@labelgoods.nl>
2016-11-21 16:26:14 +0100

    - update receive purchase order action

--- src/Marello/Bundle/PurchaseOrderBundle/Workflow/Action/ReceivePurchaseOrderAction.php
+++ src/Marello/Bundle/PurchaseOrderBundle/Workflow/Action/ReceivePurchaseOrderAction.php
@@ -3,12 +3,16 @@
 namespace Marello\Bundle\PurchaseOrderBundle\Workflow\Action;
 
 use Doctrine\Bundle\DoctrineBundle\Registry;
+
+use Symfony\Component\PropertyAccess\PropertyPathInterface;
+
+use Oro\Component\Action\Exception\InvalidParameterException;
+use Oro\Component\Action\Action\AbstractAction;
+use Oro\Component\Action\Action\ActionInterface;
+use Oro\Component\Action\Model\ContextAccessor;
+
 use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
 use Marello\Bundle\PurchaseOrderBundle\Entity\PurchaseOrder;
-use Oro\Bundle\WorkflowBundle\Exception\InvalidParameterException;
-use Oro\Bundle\WorkflowBundle\Model\Action\AbstractAction;
-use Oro\Bundle\WorkflowBundle\Model\Action\ActionInterface;
-use Oro\Bundle\WorkflowBundle\Model\ContextAccessor;
 
 class ReceivePurchaseOrderAction extends AbstractAction
 {
@@ -17,6 +21,12 @@ class ReceivePurchaseOrderAction extends AbstractAction
 
     /** @var Registry */
     protected $doctrine;
+
+    /** @var PropertyPathInterface */
+    protected $entity;
+
+    /** @var PropertyPathInterface|bool */
+    protected $isPartial;
 
     /**
      * CreateRefundAction constructor.
@@ -33,23 +43,49 @@ class ReceivePurchaseOrderAction extends AbstractAction
 
     /**
      * @param mixed $context
+     * @throws \Exception
      */
     protected function executeAction($context)
     {
-        /** @var PurchaseOrder $po */
-        $po = $context->getEntity();
-
-        $items = $po->getItems();
-
-        foreach ($items as $item) {
-            /** @var InventoryItem $inventoryItem */
-            $inventoryItem = $item->getProduct()->getInventoryItems()->first();
-
-            $inventoryItem->adjustStockLevels('purchase_order', $item->getOrderedAmount());
-            $item->setReceivedAmount($item->getOrderedAmount());
+        /** @var PurchaseOrder $purchaseOrder */
+        $purchaseOrder = $this->contextAccessor->getValue($context, $this->entity);
+        if (!$purchaseOrder) {
+            throw new \Exception('Invalid configuration of workflow action, expected entity, null given');
         }
 
+        if (!$purchaseOrder instanceof PurchaseOrder) {
+            return;
+        }
+
+        $isPartial = $this->contextAccessor->getValue($context, $this->isPartial);
+        $items = $purchaseOrder->getItems();
+        foreach ($items as $item) {
+            /** @var InventoryItem $inventoryItem */
+            if ($isPartial) {
+                file_put_contents(
+                    '/Users/hotlander/Development/marello-application-dev/app/logs/receiving.log',
+                    $item->getOrderedAmount() . "\r\n",
+                    FILE_APPEND
+                );
+
+                file_put_contents(
+                    '/Users/hotlander/Development/marello-application-dev/app/logs/receiving.log',
+                    $item->getReceivedAmount() . "\r\n",
+                    FILE_APPEND
+                );
+            } else {
+                $this->handleFullyReceived($item, $inventoryItem);
+            }
+
+        }
         $this->doctrine->getManager()->flush();
+    }
+
+    private function handleFullyReceived($item, $inventoryItem)
+    {
+        $inventoryItem = $item->getProduct()->getInventoryItems()->first();
+        $inventoryItem->adjustStockLevels('purchase_order', $item->getOrderedAmount());
+        $item->setReceivedAmount($item->getOrderedAmount());
     }
 
     /**
@@ -62,8 +98,14 @@ class ReceivePurchaseOrderAction extends AbstractAction
      */
     public function initialize(array $options)
     {
-        $this->options = $options;
+        if (!array_key_exists('entity', $options) && !$options['entity'] instanceof PropertyPathInterface) {
+            throw new InvalidParameterException('Parameter "entity" is required.');
+        } else {
+            $this->entity = $this->getOption($options, 'entity');
+        }
 
-        return $this;
+        if (array_key_exists('is_partial', $options)) {
+            $this->isPartial = $this->getOption($options, 'is_partial');
+        }
     }
 }
