cc6979a20a3fe988106dac86d6ef920e35d929c2
Hotlander <j.casteleijn@labelgoods.nl>
2016-12-28 08:54:59 +0100

    - updated OrderInventoryAllocationListener to comply with new method of updating inventory

--- src/Marello/Bundle/OrderBundle/EventListener/Doctrine/OrderInventoryAllocationListener.php
+++ src/Marello/Bundle/OrderBundle/EventListener/Doctrine/OrderInventoryAllocationListener.php
@@ -6,9 +6,10 @@ use Doctrine\ORM\Event\LifecycleEventArgs;
 
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 
-use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
 use Marello\Bundle\OrderBundle\Entity\Order;
 use Marello\Bundle\OrderBundle\Entity\OrderItem;
+use Marello\Bundle\InventoryBundle\Event\InventoryUpdateEvent;
+use Marello\Bundle\InventoryBundle\Model\InventoryUpdateContext;
 
 class OrderInventoryAllocationListener
 {
@@ -29,11 +30,40 @@ class OrderInventoryAllocationListener
         }
 
         $entity->getItems()->map(function (OrderItem $item) use ($entity, $args) {
-            /** @var InventoryItem $inventoryItem */
-            $inventoryItem = $item->getProduct()->getInventoryItems()->first();
-            $inventoryItem->adjustStockLevels('order_workflow.pending', null, $item->getQuantity(), null, $entity);
-
-            $args->getEntityManager()->persist($inventoryItem);
+            $this->handleInventoryUpdate($item, $item->getQuantity(), $entity);
         });
+    }
+
+    /**
+     * handle the inventory update for items which have been received
+     * @param OrderItem $item
+     * @param $inventoryUpdateQty
+     * @param Order $order
+     */
+    protected function handleInventoryUpdate($item, $inventoryUpdateQty, $order)
+    {
+        $inventoryItems = $item->getProduct()->getInventoryItems();
+        $inventoryItemData = [];
+        foreach ($inventoryItems as $inventoryItem) {
+            $inventoryItemData[] = [
+                'item'          => $inventoryItem,
+                'qty'           => null,
+                'allocatedQty'  => $inventoryUpdateQty
+            ];
+        }
+
+        $data = [
+            'stock'             => null,
+            'allocatedStock'    => $inventoryUpdateQty,
+            'trigger'           => 'order_workflow.pending',
+            'items'             => $inventoryItemData,
+            'relatedEntity'     => $order
+        ];
+
+        $context = InventoryUpdateContext::createUpdateContext($data);
+        $this->eventDispatcher->dispatch(
+            InventoryUpdateEvent::NAME,
+            new InventoryUpdateEvent($context)
+        );
     }
 }
