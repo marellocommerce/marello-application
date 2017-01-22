f37dacfa9e04184557822aa87c8ba094004e5a59
Hotlander <j.casteleijn@labelgoods.nl>
2017-01-22 20:48:47 +0100

    - updated SupplierSelectType grid where only active Suppliers are being shown

--- src/Marello/Bundle/SupplierBundle/Form/Type/SupplierSelectType.php
+++ src/Marello/Bundle/SupplierBundle/Form/Type/SupplierSelectType.php
@@ -14,13 +14,14 @@ class SupplierSelectType extends AbstractType
     {
         $resolver->setDefaults(
             [
-                'autocomplete_alias' => 'suppliers',
-                'create_form_route'  => 'marello_supplier_create',
-                'grid_name' => 'marello-supplier-grid',
-                'configs'            => [
-                    'placeholder' => 'marello.supplier.form.choose_supplier',
-                    'result_template_twig' => 'MarelloSupplierBundle:Supplier:Autocomplete/result.html.twig',
-                    'selection_template_twig' => 'MarelloSupplierBundle:Supplier:Autocomplete/selection.html.twig',
+                'autocomplete_alias'    => 'suppliers',
+                'create_enabled'        => false,
+                'grid_name'             => 'marello-supplier-extended-grid',
+                'grid_parameters'       => [ 'isActive' => 1],
+                'configs'               => [
+                    'placeholder'               => 'marello.supplier.form.choose_supplier',
+                    'result_template_twig'      => 'MarelloSupplierBundle:Supplier:Autocomplete/result.html.twig',
+                    'selection_template_twig'   => 'MarelloSupplierBundle:Supplier:Autocomplete/selection.html.twig',
                 ],
             ]
         );
