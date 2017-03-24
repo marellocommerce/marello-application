21f2767fe7a0c8424f198d6ab265813496ac2ba1
Joey Masip Romeu <joey.masip@gmail.com>
2017-03-24 16:20:07 +0100

    Added compiler pass to allow oro_format_currency twig filter in email templates, added mailer processor to allow images in email templates

---
+++ src/Marello/Bundle/CoreBundle/DependencyInjection/CompilerPass/TwigSandboxConfigurationPass.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Marello\Bundle\CoreBundle\DependencyInjection\CompilerPass;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+use Oro\Bundle\LocaleBundle\DependencyInjection\Compiler\TwigSandboxConfigurationPass as BaseTwigSandboxConfigurationPass;
+
+class TwigSandboxConfigurationPass implements CompilerPassInterface
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if ($container->hasDefinition(BaseTwigSandboxConfigurationPass::EMAIL_TEMPLATE_SANDBOX_SECURITY_POLICY_SERVICE_KEY) &&
+            $container->hasDefinition(BaseTwigSandboxConfigurationPass::EMAIL_TEMPLATE_RENDERER_SERVICE_KEY)
+        ) {
+            $securityPolicyDef = $container->getDefinition(BaseTwigSandboxConfigurationPass::EMAIL_TEMPLATE_SANDBOX_SECURITY_POLICY_SERVICE_KEY);
+            $filters = $securityPolicyDef->getArgument(1);
+            $filters = array_merge(
+                $filters,
+                [
+                    'oro_format_currency'
+                ]
+            );
+
+            $securityPolicyDef->replaceArgument(1, $filters);
+
+            $rendererDef = $container->getDefinition(BaseTwigSandboxConfigurationPass::EMAIL_TEMPLATE_RENDERER_SERVICE_KEY);
+            $rendererDef->addMethodCall('addExtension', [new Reference('oro_locale.twig.number')]);
+        }
+    }
+}
