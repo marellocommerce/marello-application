05781237540bcab9056aae329cb58d9c2492120b
Joey Masip Romeu <joey.masip@gmail.com>
2017-04-14 13:46:46 +0200

    Grid listener to remove workflow step in datagrids

--- src/Marello/Bundle/ReportBundle/DependencyInjection/MarelloReportExtension.php
+++ src/Marello/Bundle/ReportBundle/DependencyInjection/MarelloReportExtension.php
@@ -23,5 +23,6 @@ class MarelloReportExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
