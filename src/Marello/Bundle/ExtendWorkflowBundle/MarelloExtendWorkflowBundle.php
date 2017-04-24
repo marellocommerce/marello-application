5442bc05a93afc400346b075a0b6ad0ab7861ad6
Hotlander <j.casteleijn@labelgoods.nl>
2017-04-24 09:46:20 +0200

    - fixed override by simply using bundle inheritance instead of having mulitple services overridden.

--- src/Marello/Bundle/ExtendWorkflowBundle/MarelloExtendWorkflowBundle.php
+++ src/Marello/Bundle/ExtendWorkflowBundle/MarelloExtendWorkflowBundle.php
@@ -2,16 +2,12 @@
 
 namespace Marello\Bundle\ExtendWorkflowBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-use Marello\Bundle\ExtendWorkflowBundle\DependencyInjection\CompilerPass\TransitionButtonProviderExtensionOverridePass;
 
 class MarelloExtendWorkflowBundle extends Bundle
 {
-    public function build(ContainerBuilder $container)
+    public function getParent()
     {
-        $container->addCompilerPass(new TransitionButtonProviderExtensionOverridePass());
-        parent::build($container);
+        return 'OroWorkflowBundle';
     }
 }
