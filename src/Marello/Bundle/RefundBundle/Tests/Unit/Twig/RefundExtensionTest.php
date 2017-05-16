589f99d1bad4515e7f6227627ab2d9dfc6bb8c8c
Hotlander <j.casteleijn@labelgoods.nl>
2017-05-16 10:41:27 +0200

    - added tests for testing behaviour of isPending twig method

--- src/Marello/Bundle/RefundBundle/Tests/Unit/Twig/RefundExtensionTest.php
+++ src/Marello/Bundle/RefundBundle/Tests/Unit/Twig/RefundExtensionTest.php
@@ -4,8 +4,11 @@ namespace Marello\Bundle\RefundBundle\Tests\Unit\Twig;
 
 use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
 use Oro\Bundle\WorkflowBundle\Model\WorkflowManager;
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowItem;
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowStep;
 
 use Marello\Bundle\RefundBundle\Twig\RefundExtension;
+use Marello\Bundle\RefundBundle\Entity\Refund;
 
 class RefundExtensionTest extends WebTestCase
 {
@@ -64,5 +67,59 @@ class RefundExtensionTest extends WebTestCase
             $this->assertInstanceOf('\Twig_SimpleFunction', $function);
             $this->assertContains($function->getName(), $expectedFunctions);
         }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testRefundIsPendingIsFalse()
+    {
+        /** @var Refund $refund */
+        $refund = $this->getMockBuilder(Refund::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->workflowManager
+            ->expects($this->once())
+            ->method('getWorkflowItemsByEntity')
+            ->willReturn([]);
+
+        $this->assertFalse($this->extension->isPending($refund));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testRefundIsPendingIsTrue()
+    {
+        /** @var Refund $refund */
+        $refund = $this->getMockBuilder(Refund::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /** @var WorkflowItem $workflowItem */
+        $workflowItem = $this->getMockBuilder(WorkflowItem::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        /** @var WorkflowStep $workflowStep */
+        $workflowStep = $this->getMockBuilder(WorkflowStep::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->workflowManager
+            ->expects($this->once())
+            ->method('getWorkflowItemsByEntity')
+            ->willReturn([$workflowItem]);
+
+        $workflowItem->expects($this->atLeastOnce())
+            ->method('getCurrentStep')
+            ->willReturn($workflowStep);
+
+        $workflowStep->expects($this->atLeastOnce())
+            ->method('getName')
+            ->willReturn('pending');
+
+        $this->assertTrue($this->extension->isPending($refund));
     }
 }
