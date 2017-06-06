f8b5399ef513a5a857011ff00d0e374d20361b6f
Joey Masip Romeu <joey.masip@gmail.com>
2017-06-06 12:45:20 +0200

    Added tests for validator errors on PurchaseOrder creation

--- src/Marello/Bundle/PurchaseOrderBundle/Tests/Functional/Controller/PurchaseOrderControllerTest.php
+++ src/Marello/Bundle/PurchaseOrderBundle/Tests/Functional/Controller/PurchaseOrderControllerTest.php
@@ -3,6 +3,8 @@
 namespace Marello\Bundle\PurchaseOrderBundle\Tests\Functional\Controller;
 
 use Marello\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
+use Marello\Bundle\PurchaseOrderBundle\Form\Type\PurchaseOrderItemType;
+use Marello\Bundle\PurchaseOrderBundle\Form\Type\PurchaseOrderType;
 use Marello\Bundle\PurchaseOrderBundle\Tests\Functional\DataFixtures\LoadPurchaseOrderData;
 use Marello\Bundle\PurchaseOrderBundle\Entity\PurchaseOrder;
 use Marello\Bundle\SupplierBundle\Tests\Functional\DataFixtures\LoadSupplierData;
@@ -118,5 +120,160 @@ class PurchaseOrderControllerTest extends WebTestCase
 
         $html = $crawler->html();
         $this->assertContains('Purchase Order saved', $html);
+    }
+
+    /** @test */
+    public function testErrorSupplierCreateAction()
+    {
+        $crawler = $this->client->request('GET', $this->getUrl('marello_purchaseorder_purchaseorder_create'));
+
+        $form    = $crawler->selectButton('Continue')->form();
+        $formValues = $form->getPhpValues();
+        $formValues['input_action'] = 'marello_purchaseorder_purchaseorder_create';
+        $formValues['marello_purchase_order_create_step_one']['supplier'] = null;
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request(
+            'POST',
+            $this->getUrl('marello_purchaseorder_purchaseorder_create'),
+            $formValues);
+
+        $result = $this->client->getResponse();
+        $this->assertContains("This value should not be blank", $crawler->html());
+    }
+
+
+    /** @test */
+    public function testErrorItemsCreateAction()
+    {
+        $crawler = $this->client->request('GET', $this->getUrl('marello_purchaseorder_purchaseorder_create'));
+
+        $form    = $crawler->selectButton('Continue')->form();
+        $formValues = $form->getPhpValues();
+        $formValues['input_action'] = 'marello_purchaseorder_purchaseorder_create';
+        $supplier = $this->getReference(LoadSupplierData::SUPPLIER_1_REF);
+        $formValues['marello_purchase_order_create_step_one']['supplier'] = $supplier->getId();
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request(
+            'POST',
+            $this->getUrl('marello_purchaseorder_purchaseorder_create'),
+            $formValues);
+
+        $result = $this->client->getResponse();
+        $this->assertContains($supplier->getName(), $crawler->html());
+
+        $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
+
+        $form = $crawler->selectButton('Save and Close')->form();
+        $formValues = $form->getPhpValues();
+        $formValues['marello_purchase_order_create_step_two']['supplier'] = $supplier->getId();
+        $formValues['marello_purchase_order_create_step_two']['items'] = array();
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request($form->getMethod(), $form->getUri(), $formValues);
+
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
+
+        $html = $crawler->html();
+        $this->assertContains(PurchaseOrderType::VALIDATION_MESSAGE, $html);
+    }
+
+
+    /** @test */
+    public function testErrorProductCreateAction()
+    {
+        $crawler = $this->client->request('GET', $this->getUrl('marello_purchaseorder_purchaseorder_create'));
+
+        $form    = $crawler->selectButton('Continue')->form();
+        $formValues = $form->getPhpValues();
+        $formValues['input_action'] = 'marello_purchaseorder_purchaseorder_create';
+        $supplier = $this->getReference(LoadSupplierData::SUPPLIER_1_REF);
+        $formValues['marello_purchase_order_create_step_one']['supplier'] = $supplier->getId();
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request(
+            'POST',
+            $this->getUrl('marello_purchaseorder_purchaseorder_create'),
+            $formValues);
+
+        $result = $this->client->getResponse();
+        $this->assertContains($supplier->getName(), $crawler->html());
+
+        $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
+
+        $form = $crawler->selectButton('Save and Close')->form();
+        $formValues = $form->getPhpValues();
+        $formValues['marello_purchase_order_create_step_two']['supplier'] = $supplier->getId();
+        $formValues['marello_purchase_order_create_step_two']['items'] = array();
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1_REF);
+        $product2 = $this->getReference(LoadProductData::PRODUCT_2_REF);
+        $formValues['marello_purchase_order_create_step_two']['items'][] = array(
+            'product' => null,
+            'orderedAmount' => 4
+        );
+        $formValues['marello_purchase_order_create_step_two']['items'][] = array(
+            'product' => $product2->getId(),
+            'orderedAmount' => 5
+        );
+        $formValues['marello_purchase_order_create_step_two']['itemsAdvice']['added'] = ''. $product1->getid() . ','. $product2->getId();
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request($form->getMethod(), $form->getUri(), $formValues);
+
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
+
+        $html = $crawler->html();
+        $this->assertContains(PurchaseOrderItemType::VALIDATION_MESSAGE_PRODUCT, $html);
+    }
+
+    /** @test */
+    public function testErrorOrderedAmountCreateAction()
+    {
+        $crawler = $this->client->request('GET', $this->getUrl('marello_purchaseorder_purchaseorder_create'));
+
+        $form    = $crawler->selectButton('Continue')->form();
+        $formValues = $form->getPhpValues();
+        $formValues['input_action'] = 'marello_purchaseorder_purchaseorder_create';
+        $supplier = $this->getReference(LoadSupplierData::SUPPLIER_1_REF);
+        $formValues['marello_purchase_order_create_step_one']['supplier'] = $supplier->getId();
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request(
+            'POST',
+            $this->getUrl('marello_purchaseorder_purchaseorder_create'),
+            $formValues);
+
+        $result = $this->client->getResponse();
+        $this->assertContains($supplier->getName(), $crawler->html());
+
+        $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
+
+        $form = $crawler->selectButton('Save and Close')->form();
+        $formValues = $form->getPhpValues();
+        $formValues['marello_purchase_order_create_step_two']['supplier'] = $supplier->getId();
+        $formValues['marello_purchase_order_create_step_two']['items'] = array();
+        $product1 = $this->getReference(LoadProductData::PRODUCT_1_REF);
+        $product2 = $this->getReference(LoadProductData::PRODUCT_2_REF);
+        $formValues['marello_purchase_order_create_step_two']['items'][] = array(
+            'product' => $product1->getId(),
+            'orderedAmount' => 4
+        );
+        $formValues['marello_purchase_order_create_step_two']['items'][] = array(
+            'product' => $product2->getId(),
+            'orderedAmount' => 0
+        );
+        $formValues['marello_purchase_order_create_step_two']['itemsAdvice']['added'] = ''. $product1->getid() . ','. $product2->getId();
+
+        $this->client->followRedirects(true);
+        $crawler = $this->client->request($form->getMethod(), $form->getUri(), $formValues);
+
+        $result = $this->client->getResponse();
+        $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
+
+        $html = $crawler->html();
+        $this->assertContains(PurchaseOrderItemType::VALIDATION_MESSAGE_ORDERED_AMOUNT, $html);
     }
 }
