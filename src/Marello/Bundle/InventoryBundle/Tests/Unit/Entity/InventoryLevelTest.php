58819f51559e6f75cbfdc045e1274662a134700b
Hotlander <j.casteleijn@labelgoods.nl>
2017-11-16 16:15:23 +0100

    - fixed Inventory Unit Tests which broke during the change of InventoryItem method signature

--- src/Marello/Bundle/InventoryBundle/Tests/Unit/Entity/InventoryLevelTest.php
+++ src/Marello/Bundle/InventoryBundle/Tests/Unit/Entity/InventoryLevelTest.php
@@ -5,6 +5,7 @@ namespace Marello\Bundle\InventoryBundle\Tests\Unit\Entity;
 use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
 use Marello\Bundle\InventoryBundle\Entity\InventoryLevel;
 use Marello\Bundle\InventoryBundle\Entity\Warehouse;
+use Marello\Bundle\ProductBundle\Entity\Product;
 use Oro\Component\Testing\Unit\EntityTestCaseTrait;
 
 class InventoryLevelTest extends \PHPUnit_Framework_TestCase
@@ -16,7 +17,7 @@ class InventoryLevelTest extends \PHPUnit_Framework_TestCase
         $warehouse = new Warehouse();
         $this->assertPropertyAccessors(new InventoryLevel(), [
             ['id', 42],
-            ['inventoryItem', new InventoryItem($warehouse)],
+            ['inventoryItem', new InventoryItem($warehouse, new Product())],
             ['warehouse', $warehouse],
             ['createdAt', new \DateTime()],
             ['updatedAt', new \DateTime()]
--- src/Marello/Bundle/InventoryBundle/Tests/Unit/EventListener/InventoryUpdateEventListenerTest.php
+++ src/Marello/Bundle/InventoryBundle/Tests/Unit/EventListener/InventoryUpdateEventListenerTest.php
@@ -2,6 +2,7 @@
 
 namespace Marello\Bundle\InventoryBundle\Tests\Unit\EventListener;
 
+use Marello\Bundle\InventoryBundle\Manager\VirtualInventoryManager;
 use Marello\Bundle\InventoryBundle\Model\InventoryUpdateContext;
 use Marello\Bundle\InventoryBundle\Event\InventoryUpdateEvent;
 use Marello\Bundle\InventoryBundle\EventListener\InventoryUpdateEventListener;
@@ -25,24 +26,48 @@ class InventoryUpdateEventListenerTest extends \PHPUnit_Framework_TestCase
     protected $inventoryManager;
 
     /**
+     * @var VirtualInventoryManager|\PHPUnit_Framework_MockObject_MockObject
+     */
+    protected $virtualInventoryManager;
+
+    /**
      * {@inheritdoc}
      */
     public function setUp()
     {
-        $this->inventoryUpdateContext = $this->createMock(InventoryUpdateContext::class);
+        $this->inventoryUpdateContext = new InventoryUpdateContext();
         $this->inventoryManager = $this
             ->getMockBuilder(InventoryManager::class)
             ->disableOriginalConstructor()
             ->getMock();
 
-        $this->listener = new InventoryUpdateEventListener($this->inventoryManager);
+        $this->virtualInventoryManager = $this
+            ->getMockBuilder(VirtualInventoryManager::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->listener = new InventoryUpdateEventListener(
+            $this->inventoryManager,
+            $this->virtualInventoryManager
+        );
     }
 
     public function testHandleUpdateInventoryEvent()
     {
         $event = $this->prepareEvent();
         $this->inventoryManager->expects($this->once())
-            ->method('updateInventoryLevels')
+            ->method('updateInventoryLevel')
+            ->with($this->inventoryUpdateContext);
+
+        $this->listener->handleUpdateInventoryEvent($event);
+    }
+
+    public function testHandleEventWithVirtualInventoryContext()
+    {
+        $this->inventoryUpdateContext->setIsVirtual(true);
+        $event = $this->prepareEvent();
+        $this->virtualInventoryManager->expects($this->once())
+            ->method('updateInventoryLevel')
             ->with($this->inventoryUpdateContext);
 
         $this->listener->handleUpdateInventoryEvent($event);
