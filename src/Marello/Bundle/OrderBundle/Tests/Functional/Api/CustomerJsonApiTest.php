d69c0cdf7942b52b80f3f3e9c53072e4b55cf4f2
Hotlander <j.casteleijn@labelgoods.nl>
2018-02-14 12:59:10 +0100

    - Added additional generic test function for asserting JSON response - Added get and cget test cases for Customer API - Added aliases for customer entity - added API configuration for Customer API

---
+++ src/Marello/Bundle/OrderBundle/Tests/Functional/Api/CustomerJsonApiTest.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace Marello\Bundle\OrderBundle\Tests\Functional\Api;
+
+use Marello\Bundle\OrderBundle\Entity\Customer;
+use Symfony\Component\HttpFoundation\Response;
+
+use Marello\Bundle\CoreBundle\Tests\Functional\RestJsonApiTestCase;
+
+use Marello\Bundle\OrderBundle\Tests\Functional\DataFixtures\LoadCustomerData;
+
+class CustomerJsonApiTest extends RestJsonApiTestCase
+{
+    const TESTING_ENTITY = 'customers';
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->loadFixtures([
+            LoadCustomerData::class
+        ]);
+    }
+
+    /**
+     * Test cget (getting a list of customers) of Customer entity
+     */
+    public function testGetListOfCustomers()
+    {
+        $response = $this->cget(['entity' => self::TESTING_ENTITY], []);
+
+        $this->assertJsonResponse($response);
+        $this->assertResponseStatusCodeEquals($response, Response::HTTP_OK);
+        $this->assertResponseCount(10, $response);
+        $this->assertResponseContains('cget_customer_list.yml', $response);
+    }
+
+    /**
+     * Test get customer by id
+     */
+    public function testGetCustomerById()
+    {
+        $customer = $this->getReference('marello-customer-1');
+        $response = $this->get(
+            ['entity' => self::TESTING_ENTITY, 'id' => $customer->getId()],
+            []
+        );
+
+        $this->assertJsonResponse($response);
+        $this->assertResponseContains('get_customer_by_id.yml', $response);
+    }
+
+    /**
+     * Get a single customer by email
+     */
+    public function testGetCustomerFilteredByEmail()
+    {
+        /** @var Customer $customer */
+        $customer = $this->getReference('marello-customer-1');
+        $response = $this->cget(
+            ['entity' => self::TESTING_ENTITY],
+            [
+                'filter' => ['email' =>  $customer->getEmail() ]
+            ]
+        );
+
+        $this->assertJsonResponse($response);
+        $this->assertResponseCount(1, $response);
+        $this->assertResponseContains('get_customer_by_email.yml', $response);
+    }
+
+    /**
+     * Create a new customer
+     */
+    public function testCreateNewCustomer()
+    {
+        $requestData = [
+            'data' => [
+                'type' => self::TESTING_ENTITY,
+                'attributes' => [
+                    'firstName' => 'John',
+                    'lastName'  => 'Doe',
+                    'email'     => 'new_customer@example.com',
+                ],
+//                'relationships' => [
+//                    'primaryAddress' => [
+//                        'data' => [
+//                            'type' => 'marello_addresses',
+//                            'id'   => '2'
+//                        ]
+//                    ]
+//                ],
+                'included' => [
+                    'type' => 'marello_addresses',
+                    'attributes' =>  [
+                            'firstName'  => 'John',
+                            'lastName'   => 'Doe',
+                            'country'    => 'NL',
+                            'street'     => 'Torenallee 20',
+                            'city'       => 'Eindhoven',
+                            'region'     => 'NL-NB',
+                            'postalCode' => '5617 BC',
+                            'company'    => 'Madia Inc'
+                    ]
+                ]
+            ]
+        ];
+
+        $response = $this->post(
+            ['entity' => self::TESTING_ENTITY],
+            $requestData
+        );
+
+        $this->assertJsonResponse($response);
+
+        $responseContent = json_decode($response->getContent());
+
+        /** @var Customer $customer */
+        $customer = $this->getEntityManager()->find(Customer::class, $responseContent->data->id);
+        $this->assertEquals($customer->getEmail(), $responseContent->data->attributes->email);
+
+        $address = $customer->getPrimaryAddress();
+        var_dump($address->getId());
+        var_dump($address->getCity());
+    }
+}
---
+++ src/Marello/Bundle/OrderBundle/Tests/Functional/Api/OrderJsonApiTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Marello\Bundle\OrderBundle\Tests\Functional\Controller\Api\Rest;
+
+use Oro\Bundle\ApiBundle\Tests\Functional\RestJsonApiTestCase;
+
+class OrderJsonApiTest extends RestJsonApiTestCase
+{
+
+}
