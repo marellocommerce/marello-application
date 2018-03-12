5b0be99053adb28ad610aa80097fde10a61ce943
Hotlander <j.casteleijn@labelgoods.nl>
2018-03-12 16:55:19 +0100

    - Updated Customer tests with updating with and without address - Added doc for customer api spec

--- src/Marello/Bundle/OrderBundle/Tests/Functional/Api/CustomerJsonApiTest.php
+++ src/Marello/Bundle/OrderBundle/Tests/Functional/Api/CustomerJsonApiTest.php
@@ -74,7 +74,26 @@ class CustomerJsonApiTest extends RestJsonApiTestCase
     {
         $response = $this->post(
             ['entity' => self::TESTING_ENTITY],
-            'customer_create.yml'
+            'customer_create_with_address.yml'
+        );
+
+        $this->assertJsonResponse($response);
+
+        $responseContent = json_decode($response->getContent());
+
+        /** @var Customer $customer */
+        $customer = $this->getEntityManager()->find(Customer::class, $responseContent->data->id);
+        $this->assertEquals($customer->getEmail(), $responseContent->data->attributes->email);
+    }
+
+    /**
+     * Create a new customer
+     */
+    public function testCreateNewCustomerWithoutAddress()
+    {
+        $response = $this->post(
+            ['entity' => self::TESTING_ENTITY],
+            'customer_create_without_address.yml'
         );
 
         $this->assertJsonResponse($response);
@@ -106,5 +125,34 @@ class CustomerJsonApiTest extends RestJsonApiTestCase
         /** @var Customer $customer */
         $customer = $this->getEntityManager()->find(Customer::class, $responseContent->data->id);
         $this->assertEquals($customer->getEmail(), 'mynewemailaddres@example.com');
+    }
+
+    /**
+     * Update existing customer email address
+     */
+    public function testUpdateAddressExistingCustomer()
+    {
+        /** @var Customer $existingCustomer */
+        $existingCustomer = $this->getReference('marello-customer-1');
+        $response = $this->patch(
+            [
+                'entity' => self::TESTING_ENTITY,
+                'id' => $existingCustomer->getId(),
+                'association' => 'marelloaddresses',
+            ],
+            'customer_address_update.yml'
+        );
+
+        $this->assertJsonResponse($response);
+        $responseContent = json_decode($response->getContent());
+        /** @var Customer $customer */
+        $customer = $this->getEntityManager()->find(Customer::class, $responseContent->data->id);
+        $primaryAddress = $customer->getPrimaryAddress();
+        self::assertSame('1215 Caldwell Road', $primaryAddress->getStreet());
+        self::assertSame('Rochester', $primaryAddress->getCity());
+        self::assertSame('777-777-777', $primaryAddress->getPhone());
+        self::assertSame('14608', $primaryAddress->getPostalCode());
+        self::assertSame('US', $primaryAddress->getCountryIso2());
+        self::assertSame('US-NY', $primaryAddress->getRegion()->getCombinedCode());
     }
 }
