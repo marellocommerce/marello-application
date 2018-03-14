884ac5f8c37569324c94dd3fad1c5c8e66c796d2
Hotlander <j.casteleijn@labelgoods.nl>
2018-03-14 16:49:20 +0100

    - Added JSON Api support for getting products and filtering products - Added tests for Product JSON Api

---
+++ src/Marello/Bundle/ProductBundle/Tests/Functional/Api/ProductJsonApiTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Marello\Bundle\ProductBundle\Tests\Functional\Api;
+
+use Symfony\Component\HttpFoundation\Response;
+
+use Marello\Bundle\ProductBundle\Entity\Product;
+use Marello\Bundle\CoreBundle\Tests\Functional\RestJsonApiTestCase;
+use Marello\Bundle\ProductBundle\Tests\Functional\DataFixtures\LoadProductData;
+
+class ProductJsonApiTest extends RestJsonApiTestCase
+{
+    const TESTING_ENTITY = 'products';
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->loadFixtures([
+            LoadProductData::class
+        ]);
+    }
+
+    /**
+     * Test cget (getting a list of products) of Product entity
+     */
+    public function testGetListOfProducts()
+    {
+        $response = $this->cget(['entity' => self::TESTING_ENTITY], []);
+
+        $this->assertJsonResponse($response);
+        $this->assertResponseStatusCodeEquals($response, Response::HTTP_OK);
+        $this->assertResponseCount(4, $response);
+        $this->assertResponseContains('cget_product_list.yml', $response);
+    }
+
+    /**
+     * Test get product by id
+     */
+    public function testGetProductById()
+    {
+        /** @var Product $product */
+        $product = $this->getReference(LoadProductData::PRODUCT_1_REF);
+        $response = $this->get(
+            ['entity' => self::TESTING_ENTITY, 'id' => $product->getId()],
+            []
+        );
+
+        $this->assertJsonResponse($response);
+        $this->assertResponseContains('get_product_by_id.yml', $response);
+    }
+
+    /**
+     * Get a single product by sku
+     */
+    public function testGetProductFilteredBySku()
+    {
+        /** @var Product $product */
+        $product = $this->getReference(LoadProductData::PRODUCT_1_REF);
+        $response = $this->cget(
+            ['entity' => self::TESTING_ENTITY],
+            [
+                'filter' => ['sku' =>  $product->getSku() ]
+            ]
+        );
+
+        $this->assertJsonResponse($response);
+        $this->assertResponseCount(1, $response);
+        $this->assertResponseContains('get_product_by_sku.yml', $response);
+    }
+}
