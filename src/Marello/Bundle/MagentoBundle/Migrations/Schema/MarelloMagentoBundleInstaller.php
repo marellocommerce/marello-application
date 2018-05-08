925248a6fdfa20f6f8f54d1f14d3128398536b61
oz muhsin <omar@madia.nl>
2018-05-08 11:46:13 +0200

    MAR10001-10;created, updated at

--- src/Marello/Bundle/MagentoBundle/Migrations/Schema/MarelloMagentoBundleInstaller.php
+++ src/Marello/Bundle/MagentoBundle/Migrations/Schema/MarelloMagentoBundleInstaller.php
@@ -78,7 +78,7 @@ class MarelloMagentoBundleInstaller implements
      */
     public function up(Schema $schema, QueryBag $queries)
     {
-//        $this->updateIntegrationTransportTable($schema);
+        $this->updateIntegrationTransportTable($schema);
 
         /** Tables generation **/
         $this->createMarelloMagentoWebsiteTable($schema);
@@ -198,8 +198,8 @@ class MarelloMagentoBundleInstaller implements
             ['notnull' => false]
         );
         $table->addColumn('price', 'money', ['notnull' => false]);
-//        $table->addColumn('created_at', 'datetime');
-//        $table->addColumn('updated_at', 'datetime');
+        $table->addColumn('created_at', 'datetime');
+        $table->addColumn('updated_at', 'datetime');
         $table->addColumn('origin_id', 'integer', ['unsigned' => true]);
         $table->addColumn('cost', 'money', ['notnull' => false]);
         $table->addIndex(['channel_id'], 'IDX_5A17298272F5A1AA', []);
