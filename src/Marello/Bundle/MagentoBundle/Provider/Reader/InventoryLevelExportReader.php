b4d172b0af3179f71e536f9110fdbd0015221af4
oz muhsin <omar@madia.nl>
2018-06-07 14:03:04 +0200

    MAR10001-10;cleanup

--- src/Marello/Bundle/MagentoBundle/Provider/Reader/InventoryLevelExportReader.php
+++ src/Marello/Bundle/MagentoBundle/Provider/Reader/InventoryLevelExportReader.php
@@ -3,12 +3,8 @@
 namespace Marello\Bundle\MagentoBundle\Provider\Reader;
 
 use Doctrine\ORM\Query\Expr\Join;
-use Marello\Bundle\MagentoBundle\Provider\MagentoChannelType;
-use Psr\Log\LoggerAwareInterface;
-use Psr\Log\LoggerAwareTrait;
 
 use Oro\Bundle\OrganizationBundle\Entity\Organization;
-use Oro\Bundle\ImportExportBundle\Reader\EntityReader;
 
 class InventoryLevelExportReader extends AbstractExportReader
 {
@@ -27,8 +23,11 @@ class InventoryLevelExportReader extends AbstractExportReader
     /**
      * {@inheritdoc}
      */
-    protected function createSourceEntityQueryBuilder($entityName, Organization $organization = null, array $ids = [])
-    {
+    protected function createSourceEntityQueryBuilder(
+        $entityName,
+        Organization $organization = null,
+        array $ids = []
+    ) {
         $qb = parent::createSourceEntityQueryBuilder($entityName, $organization, $ids);
 
         $qb->innerJoin(
