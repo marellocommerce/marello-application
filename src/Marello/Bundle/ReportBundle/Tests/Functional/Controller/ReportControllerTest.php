c330791ea51ad776666268650fb74493c9c2a092
Hotlander <j.casteleijn@labelgoods.nl>
2018-06-19 12:16:34 +0200

    - Fixed broken test of checking the report; Test contained old naming of the datagrid

--- src/Marello/Bundle/ReportBundle/Tests/Functional/Controller/ReportControllerTest.php
+++ src/Marello/Bundle/ReportBundle/Tests/Functional/Controller/ReportControllerTest.php
@@ -11,7 +11,7 @@ class ReportControllerTest extends WebTestCase
     const ORDERS_GRID_NAME = 'marello_report-orders';
 
     const PRODUCTS_REPORT_GROUP = 'products';
-    const PRODUCTS_REPORT_NAME = 'low_stock_products';
+    const PRODUCTS_REPORT_NAME = 'low_inventory_products';
     const PRODUCTS_GRID_NAME = 'marello_report-products';
 
     public function setUp()
@@ -84,7 +84,7 @@ class ReportControllerTest extends WebTestCase
                 self::PRODUCTS_GRID_NAME,
                 self::PRODUCTS_REPORT_NAME,
                 self::PRODUCTS_REPORT_GROUP,
-                'Low Stock Products'
+                'Low Inventory Products'
             ],
         ];
     }
