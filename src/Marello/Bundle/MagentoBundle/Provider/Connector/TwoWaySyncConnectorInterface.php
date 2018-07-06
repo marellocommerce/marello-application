a94544990607c92a4607cd12cc0bb1e1a5ddfbb4
oz muhsin <omar@madia.nl>
2018-07-06 14:25:31 +0200

    MAR10001-389;doc update

--- src/Marello/Bundle/MagentoBundle/Provider/Connector/TwoWaySyncConnectorInterface.php
+++ src/Marello/Bundle/MagentoBundle/Provider/Connector/TwoWaySyncConnectorInterface.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: muhsin
- * Date: 18/05/2018
- * Time: 10:33
- */
-
 namespace Marello\Bundle\MagentoBundle\Provider\Connector;
 
 use \Oro\Bundle\IntegrationBundle\Provider\TwoWaySyncConnectorInterface as BaseTwoWaySyncConnectorInterface;
