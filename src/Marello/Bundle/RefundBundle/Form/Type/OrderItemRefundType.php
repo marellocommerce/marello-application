37ceec2733f2c211f9eb729ef64563845a2bb4f3
Hotlander <j.casteleijn@labelgoods.nl>
2018-12-19 12:45:03 +0100

    - Fixed with using the same class twice in a single class - Updated data for testing Order requests where shippingmethod and shipping method type are required

--- src/Marello/Bundle/RefundBundle/Form/Type/OrderItemRefundType.php
+++ src/Marello/Bundle/RefundBundle/Form/Type/OrderItemRefundType.php
@@ -3,7 +3,6 @@
 namespace Marello\Bundle\RefundBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\MoneyType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
