a762d0239c4bf52d409db6f83a7e4c93970305d9
Hotlander <j.casteleijn@labelgoods.nl>
2019-04-18 15:15:49 +0200

    - Added some additional tests for Product attributes and families

--- src/Marello/Bundle/ProductBundle/Tests/Functional/Controller/ProductAttributeControllerTest.php
+++ src/Marello/Bundle/ProductBundle/Tests/Functional/Controller/ProductAttributeControllerTest.php
@@ -4,45 +4,46 @@ namespace Marello\Bundle\ProductBundle\Tests\Functional\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
-use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Oro\Bundle\EntityConfigBundle\Tests\Functional\Controller\AttributeControllerTest;
 
 /**
  * @outputBuffering enabled
  */
-class ProductAttributeControllerTest extends WebTestCase
+class ProductAttributeControllerTest extends AttributeControllerTest
 {
     const PRODUCT_ENTITY_ALIAS = 'marelloproduct';
 
-    public function setUp()
-    {
-        $this->initClient(
-            [],
-            array_merge($this->generateBasicAuthHeader(), ['HTTP_X-CSRF-Header' => 1])
-        );
-    }
-
     /**
-     * just checking wether we are getting a 200 result back and not an error when navigating to this route
-     * {@inheritdoc}
-     */
-    public function testProductAttributeFamilyIndex()
-    {
-        $this->client->request(
-            'GET',
-            $this->getUrl('oro_attribute_family_index', ['alias' => self::PRODUCT_ENTITY_ALIAS])
-        );
-        $result = $this->client->getResponse();
-        $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
-    }
-
-    /**
-     * just checking wether we are getting a 200 result back and not an error when navigating to this route
-     * {@inheritdoc}
+     * just checking whether we are getting a 200 result back and not an error when navigating to this route
      */
     public function testProductAttributeIndex()
     {
         $this->client->request('GET', $this->getUrl('oro_attribute_index', ['alias' => self::PRODUCT_ENTITY_ALIAS]));
         $result = $this->client->getResponse();
         $this->assertHtmlResponseStatusCodeEquals($result, Response::HTTP_OK);
+    }
+
+    /**
+     * @return string
+     */
+    protected function getTestEntityAlias()
+    {
+        return self::PRODUCT_ENTITY_ALIAS;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testCreateImage()
+    {
+        // skip test during hardcoded entity classname in dependent functions
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function testCreateFile()
+    {
+        // skip test during hardcoded entity classname in dependent functions
     }
 }
