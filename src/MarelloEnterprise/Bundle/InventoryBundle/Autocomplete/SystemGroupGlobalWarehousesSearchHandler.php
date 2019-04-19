84cf52f4ae4871c275f01ce1f7f75430efc12e74
Hotlander <j.casteleijn@labelgoods.nl>
2019-04-19 14:12:57 +0200

    - Updated search handler to include external warehouses in order to show external wh's when creating and editing warehouse groups

--- src/MarelloEnterprise/Bundle/InventoryBundle/Autocomplete/SystemGroupGlobalWarehousesSearchHandler.php
+++ src/MarelloEnterprise/Bundle/InventoryBundle/Autocomplete/SystemGroupGlobalWarehousesSearchHandler.php
@@ -74,7 +74,8 @@ class SystemGroupGlobalWarehousesSearchHandler extends SearchHandler
                 ->setParameter('isSystem', true)
                 ->setParameter('types', [
                     WarehouseTypeProviderInterface::WAREHOUSE_TYPE_GLOBAL,
-                    WarehouseTypeProviderInterface::WAREHOUSE_TYPE_VIRTUAL
+                    WarehouseTypeProviderInterface::WAREHOUSE_TYPE_VIRTUAL,
+                    WarehouseTypeProviderInterface::WAREHOUSE_TYPE_EXTERNAL
                 ])
                 ->setParameter('id', $groupId);
         }
