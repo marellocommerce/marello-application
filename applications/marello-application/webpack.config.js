815e5ee1f03bf33ffe1a6babfa2d2e97ac7cf77c
Hotlander <j.casteleijn@labelgoods.nl>
2019-05-02 11:46:10 +0200

    Merge commit '11ab45ce3c32e2982aa62cb56e7336732d8baa04' as 'package/marello-enterprise'

--- applications/marello-application/webpack.config.js
+++
@@ -1,7 +0,0 @@
-const OroConfig = require('./vendor/oro/platform/build/webpack-config-builder/oro-webpack-config');
-
-OroConfig
-    .setPublicPath('public/')
-    .setCachePath('var/cache');
-
-module.exports = OroConfig.getWebpackConfig();
