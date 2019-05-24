22a02abd2e26d568767c7824dba7970017871107
Volodymyr Denchyk <devit_dvv@ukr.net>
2019-05-24 15:49:35 +0300

    MAR10001-499: Filtering SalesChannel(s) on product edit page's SalesChannel selection grid

--- src/Marello/Bundle/FilterBundle/Grid/Extension/OrmFilterExtension.php
+++ src/Marello/Bundle/FilterBundle/Grid/Extension/OrmFilterExtension.php
@@ -3,39 +3,36 @@
 namespace Marello\Bundle\FilterBundle\Grid\Extension;
 
 use Oro\Bundle\DataGridBundle\Datagrid\Common\DatagridConfiguration;
-use Oro\Bundle\DataGridBundle\Datagrid\Common\MetadataObject;
-use Oro\Bundle\DataGridBundle\Extension\Appearance\AppearanceExtension;
-use Oro\Bundle\DataGridBundle\Extension\Pager\PagerInterface;
-use Oro\Bundle\DataGridBundle\Extension\Sorter\AbstractSorterExtension;
-use Oro\Bundle\FilterBundle\Filter\FilterUtility;
 use Oro\Bundle\FilterBundle\Grid\Extension\Configuration;
 use Oro\Bundle\FilterBundle\Grid\Extension\OrmFilterExtension as BaseOrmFilterExtension;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class OrmFilterExtension extends BaseOrmFilterExtension
 {
     /**
+     * @var RequestStack
+     */
+    private $requestStack;
+
+    /**
+     * @param RequestStack $requestStack
+     */
+    public function setRequestStack(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    /**
      * {@inheritDoc}
      */
-    protected function getValuesToApply(DatagridConfiguration $config, $readParameters = true)
+    public function processConfigs(DatagridConfiguration $config)
     {
-        $defaultFilters = $config->offsetGetByPath(Configuration::DEFAULT_FILTERS_PATH, []);
-
-        if (!$readParameters) {
-            return $defaultFilters;
-        } else {
-            $parameters = $this->getParameters();
-            $currentFilters = $parameters->get(self::FILTER_ROOT_PARAM, []);
-            if ($currentFilters === null) {
-                $currentFilters = [];
-            }
-            $pager = $parameters->get(PagerInterface::PAGER_ROOT_PARAM, []);
-            $appearance = $parameters->get(AppearanceExtension::APPEARANCE_ROOT_PARAM, []);
-            $sorter = $parameters->get(AbstractSorterExtension::SORTERS_ROOT_PARAM, []);
-            if (!empty($pager) || !empty($appearance) || !empty($sorter)) {
-                return $currentFilters;
-            } else {
-                return array_replace($defaultFilters, $currentFilters);
-            }
+        parent::processConfigs($config);
+        $currentRequest = $this->requestStack->getCurrentRequest();
+        if ($currentRequest && $currentRequest->isXmlHttpRequest()) {
+            $filters = $config->offsetGetByPath(Configuration::FILTERS_PATH);
+            unset($filters['default']);
+            $config->offsetSetByPath(Configuration::FILTERS_PATH, $filters);
         }
     }
 }
