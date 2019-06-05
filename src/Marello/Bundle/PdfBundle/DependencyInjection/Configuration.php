27b3ecf9862d66ec21cd339c609b2f0c44277491
Geert Lamers <geert@madia.nl>
2019-06-05 15:51:57 +0200

    MAR10001-13; Add MPDF composer dependency

--- src/Marello/Bundle/PdfBundle/DependencyInjection/Configuration.php
+++ src/Marello/Bundle/PdfBundle/DependencyInjection/Configuration.php
@@ -2,20 +2,24 @@
 
 namespace Marello\Bundle\PdfBundle\DependencyInjection;
 
+use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
 class Configuration implements ConfigurationInterface
 {
-    /**
-     * Generates the configuration tree builder.
-     *
-     * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder The tree builder
-     */
+    const CONFIG_NAME = 'marello_pdf';
+
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('marello_pdf');
+
+        $rootNode = $treeBuilder->root(self::CONFIG_NAME);
 
         return $treeBuilder;
     }
--- src/Marello/Bundle/PdfBundle/DependencyInjection/MarelloPdfExtension.php
+++ src/Marello/Bundle/PdfBundle/DependencyInjection/MarelloPdfExtension.php
@@ -2,27 +2,29 @@
 
 namespace Marello\Bundle\PdfBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
+/**
+ * This is the class that loads and manages your bundle configuration.
+ *
+ * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
+ */
 class MarelloPdfExtension extends Extension
 {
     /**
-     * Loads a specific configuration.
-     *
-     * @param array            $configs   An array of configuration values
-     * @param ContainerBuilder $container A ContainerBuilder instance
-     *
-     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        $container->prependExtensionConfig(Configuration::CONFIG_NAME, $config);
     }
 }
