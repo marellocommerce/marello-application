2bf51133df85c58f83343a10c871cb682e10d933
Geert Lamers <geert@madia.nl>
2019-06-13 18:03:26 +0200

    MAR10001-13; Add configuration for values on PDF

--- src/Marello/Bundle/PdfBundle/DependencyInjection/Configuration.php
+++ src/Marello/Bundle/PdfBundle/DependencyInjection/Configuration.php
@@ -15,11 +15,32 @@ class Configuration implements ConfigurationInterface
 {
     const CONFIG_NAME = 'marello_pdf';
 
+    const CONFIG_KEY_LANGUAGE = 'language';
+    const CONFIG_KEY_LOCALE = 'locale';
+    const CONFIG_KEY_COMPANY_ADDRESS = 'company_address';
+    const CONFIG_KEY_COMPANY_EMAIL = 'company_email';
+    const CONFIG_KEY_COMPANY_PHONE = 'company_phone';
+    const CONFIG_KEY_COMPANY_BANK = 'company_bank';
+    const CONFIG_KEY_COMPANY_COC = 'company_coc';
+
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
 
         $rootNode = $treeBuilder->root(self::CONFIG_NAME);
+
+        SettingsBuilder::append(
+            $rootNode,
+            [
+                self::CONFIG_KEY_LANGUAGE => ['value' => 'en'],
+                self::CONFIG_KEY_LOCALE => ['value' => 'en'],
+                self::CONFIG_KEY_COMPANY_ADDRESS => ['value' => null],
+                self::CONFIG_KEY_COMPANY_EMAIL => ['value' => null],
+                self::CONFIG_KEY_COMPANY_PHONE => ['value' => null],
+                self::CONFIG_KEY_COMPANY_BANK => ['value' => null],
+                self::CONFIG_KEY_COMPANY_COC => ['value' => null],
+            ]
+        );
 
         return $treeBuilder;
     }
