0691eaa1ee286a870ee9ce5a7e8931f1adf89209
Hotlander <j.casteleijn@labelgoods.nl>
2019-07-08 16:47:59 +0200

    - Removed inventory check when subscription product is in the order and the OrderItem status is being updated

--- package/marello-subscriptions/src/Marello/Bundle/SubscriptionBundle/EventListener/OrderItemStatusUpdateListener.php
+++ package/marello-subscriptions/src/Marello/Bundle/SubscriptionBundle/EventListener/OrderItemStatusUpdateListener.php
@@ -16,23 +16,7 @@ class OrderItemStatusUpdateListener
         $item = $event->getOrderItem();
         $product = $item->getProduct();
         if ($product->getType() === LoadOrderItemSubscriptionStatus::SUBSCRIPTION) {
-            if ($this->hasInventory($product) === false) {
-                $event->setStatusName(LoadOrderItemSubscriptionStatus::SUBSCRIPTION);
-            }
+            $event->setStatusName(LoadOrderItemSubscriptionStatus::SUBSCRIPTION);
         }
-    }
-    
-    private function hasInventory(Product $product)
-    {
-        if (count($product->getInventoryItems()) === 0) {
-            return false;
-        }
-        foreach ($product->getInventoryItems() as $inventoryItem) {
-            if (count($inventoryItem->getInventoryLevels()) > 0) {
-                return true;
-            }
-        }
-        
-        return false;
     }
 }
