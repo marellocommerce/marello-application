53ce0d6f0e9e1b9ea5dd171dd24605b7f39589e4
Hotlander <j.casteleijn@labelgoods.nl>
2019-07-08 16:47:59 +0200

    - Removed inventory check when subscription product is in the order and the OrderItem status is being updated

--- src/Marello/Bundle/OrderBundle/EventListener/Doctrine/OrderItemStatusListener.php
+++ src/Marello/Bundle/OrderBundle/EventListener/Doctrine/OrderItemStatusListener.php
@@ -64,6 +64,7 @@ class OrderItemStatusListener
                 $product,
                 $entity->getOrder()->getSalesChannel()
             );
+            // add check for the 'quantity' that is not numeric (a.k.a. no qty set for back-and pre-order)
             if ($availableInventory < $entity->getQuantity() &&
                 (
                     ($inventoryItem->isBackorderAllowed() &&
