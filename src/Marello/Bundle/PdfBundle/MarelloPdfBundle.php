a7d0a5c5a6ff23a3efafd116b69656215247d92b
Geert Lamers <geert@madia.nl>
2019-07-09 17:55:09 +0200

    MAR10001-13; Add DocumentTableProviderPass

--- src/Marello/Bundle/PdfBundle/MarelloPdfBundle.php
+++ src/Marello/Bundle/PdfBundle/MarelloPdfBundle.php
@@ -2,6 +2,7 @@
 
 namespace Marello\Bundle\PdfBundle;
 
+use Marello\Bundle\PdfBundle\DependencyInjection\CompilerPass\DocumentTableProviderPass;
 use Marello\Bundle\PdfBundle\DependencyInjection\CompilerPass\RenderParameterProviderPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -13,5 +14,6 @@ class MarelloPdfBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new RenderParameterProviderPass());
+        $container->addCompilerPass(new DocumentTableProviderPass());
     }
 }
