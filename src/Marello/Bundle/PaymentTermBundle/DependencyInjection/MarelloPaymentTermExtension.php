c8779e49c433a316f2d6e28fe0a11bb71d47e4a6
Geert Lamers <geert@madia.nl>
2019-07-24 16:47:41 +0200

    MAR10001-701; Add default payment term configuration

--- src/Marello/Bundle/PaymentTermBundle/DependencyInjection/MarelloPaymentTermExtension.php
+++ src/Marello/Bundle/PaymentTermBundle/DependencyInjection/MarelloPaymentTermExtension.php
@@ -7,15 +7,34 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
 class MarelloPaymentTermExtension extends Extension
 {
+    const ALIAS = 'marello_payment_term';
+
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function load(array $config, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+        $container->prependExtensionConfig($this->getAlias(), $config);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('form.yml');
         $loader->load('services.yml');
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getAlias()
+    {
+        return self::ALIAS;
     }
 }
