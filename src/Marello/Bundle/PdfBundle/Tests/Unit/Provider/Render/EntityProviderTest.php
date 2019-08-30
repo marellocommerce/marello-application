88ca77277dd4fd23e2bfb6935a5462fcf5381d68
Geert Lamers <geert@madia.nl>
2019-08-30 15:23:00 +0200

    MAR10001-13; PdfBundle unit tests

---
+++ src/Marello/Bundle/PdfBundle/Tests/Unit/Provider/Render/EntityProviderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Marello\Bundle\PdfBundle\Tests\Unit\Provider\Render;
+
+use Marello\Bundle\PdfBundle\Provider\Render\EntityProvider;
+use PHPUnit\Framework\TestCase;
+
+class EntityProviderTest extends TestCase
+{
+    protected $provider;
+
+    public function setUp()
+    {
+        $this->provider = new EntityProvider();
+    }
+
+    public function testSupports()
+    {
+        $this->assertTrue($this->provider->supports('test value', []));
+    }
+
+    public function testGetParams()
+    {
+        $result = $this->provider->getParams('test value', []);
+
+        $this->assertTrue(is_array($result));
+        $this->assertEquals('test value', $result['entity']);
+    }
+}
