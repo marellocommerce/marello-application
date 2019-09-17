2b266c8f615cfb2e59ed1f72e54c60a4d044c493
Hotlander <j.casteleijn@labelgoods.nl>
2019-09-17 12:15:26 +0200

    - remove unused code from validator

--- src/Marello/Bundle/InventoryBundle/Validator/InventoryOrderOnDemandValidator.php
+++ src/Marello/Bundle/InventoryBundle/Validator/InventoryOrderOnDemandValidator.php
@@ -2,39 +2,14 @@
 
 namespace Marello\Bundle\InventoryBundle\Validator;
 
-use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
-use Marello\Bundle\OrderBundle\Event\ProductAvailableInventoryValidationEvent;
-use Marello\Bundle\ProductBundle\Entity\Product;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 use Symfony\Component\Validator\Exception\UnexpectedTypeException;
-use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
-use Symfony\Component\PropertyAccess\PropertyAccess;
 
-use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
-
-use Marello\Bundle\OrderBundle\Entity\Order;
-use Marello\Bundle\ProductBundle\Entity\ProductInterface;
-use Marello\Bundle\InventoryBundle\Provider\AvailableInventoryProvider;
-use Marello\Bundle\OrderBundle\Validator\Constraints\AvailableInventory;
+use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
 
 class InventoryOrderOnDemandValidator extends ConstraintValidator
 {
-    /**
-     * @var DoctrineHelper
-     */
-    private $doctrineHelper;
-
-    /**
-     * @param DoctrineHelper $doctrineHelper
-     */
-    public function __construct(
-        DoctrineHelper $doctrineHelper
-    ) {
-        $this->doctrineHelper = $doctrineHelper;
-    }
-
     /**
      * Checks if the passed entity is unique in collection.
      * @param mixed $entity
