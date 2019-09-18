dc861c5877e11de32f2f29e9357921e7867a47b3
Geert Lamers <geert@madia.nl>
2019-09-18 16:01:06 +0200

    MAR10001-742; Add e-mail settings about sending invoices

--- src/Marello/Bundle/PdfBundle/DependencyInjection/Configuration.php
+++ src/Marello/Bundle/PdfBundle/DependencyInjection/Configuration.php
@@ -24,6 +24,10 @@ class Configuration implements ConfigurationInterface
     const CONFIG_KEY_COMPANY_PHONE = 'company_phone';
     const CONFIG_KEY_COMPANY_BANK = 'company_bank';
     const CONFIG_KEY_COMPANY_COC = 'company_coc';
+    const CONFIG_KEY_EMAIL_WORKFLOW_TRANSITION = 'email_workflow_transition';
+    const CONFIG_KEY_EMAIL_SENDER_NAME = 'email_sender_name';
+    const CONFIG_KEY_EMAIL_SENDER_EMAIL = 'email_sender_email';
+    const CONFIG_KEY_EMAIL_BCC = 'email_bcc';
 
     const PAPER_SIZE_A4 = 'a4';
     const PAPER_SIZE_LETTER = 'letter';
@@ -46,6 +50,10 @@ class Configuration implements ConfigurationInterface
                 self::CONFIG_KEY_COMPANY_PHONE => ['value' => null],
                 self::CONFIG_KEY_COMPANY_BANK => ['value' => null],
                 self::CONFIG_KEY_COMPANY_COC => ['value' => null],
+                self::CONFIG_KEY_EMAIL_WORKFLOW_TRANSITION => ['value' => null],
+                self::CONFIG_KEY_EMAIL_SENDER_NAME => ['value' => null],
+                self::CONFIG_KEY_EMAIL_SENDER_EMAIL => ['value' => null],
+                self::CONFIG_KEY_EMAIL_BCC => ['value' => null],
             ]
         );
 
