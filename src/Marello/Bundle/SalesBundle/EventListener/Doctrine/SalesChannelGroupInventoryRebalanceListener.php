f22f39a1d27b95044d66fad4d40d5c97ce605386
Hotlander <j.casteleijn@labelgoods.nl>
2019-09-19 15:42:21 +0200

    - removed the trigger of a rebalance when installing to reduce additional messages in queue - refactored the trigger of rebalancing when a SalesChannelGroups are created or deleted to only rebalance products linked to those SalesChannelGroups

--- src/Marello/Bundle/SalesBundle/EventListener/Doctrine/SalesChannelGroupInventoryRebalanceListener.php
+++ src/Marello/Bundle/SalesBundle/EventListener/Doctrine/SalesChannelGroupInventoryRebalanceListener.php
@@ -5,9 +5,12 @@ namespace Marello\Bundle\SalesBundle\EventListener\Doctrine;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\UnitOfWork;
+
+use Oro\Component\MessageQueue\Client\MessageProducerInterface;
+
+use Marello\Bundle\ProductBundle\Entity\Product;
 use Marello\Bundle\InventoryBundle\Async\Topics;
 use Marello\Bundle\SalesBundle\Entity\SalesChannelGroup;
-use Oro\Component\MessageQueue\Client\MessageProducerInterface;
 
 class SalesChannelGroupInventoryRebalanceListener
 {
@@ -36,23 +39,40 @@ class SalesChannelGroupInventoryRebalanceListener
     }
 
     /**
-     * Handle incoming event
+     * {@inheritdoc}
      * @param OnFlushEventArgs $eventArgs
+     * @throws \Exception
      */
     public function onFlush(OnFlushEventArgs $eventArgs)
     {
         $this->em = $eventArgs->getEntityManager();
         $this->unitOfWork = $this->em->getUnitOfWork();
-
+        $records = [];
         if (!empty($this->unitOfWork->getScheduledEntityInsertions())) {
-            if (!empty($this->filterRecords($this->unitOfWork->getScheduledEntityInsertions()))) {
-                $this->triggerRebalance();
-            }
+            $records = array_merge($records, $this->filterRecords($this->unitOfWork->getScheduledEntityInsertions()));
         }
+
         if (!empty($this->unitOfWork->getScheduledEntityDeletions())) {
-            if (!empty($this->filterRecords($this->unitOfWork->getScheduledEntityDeletions()))) {
-                $this->triggerRebalance();
-            }
+            $records = array_merge($records, $this->filterRecords($this->unitOfWork->getScheduledEntityDeletions()));
+        }
+
+        if (!empty($records)) {
+            $this->applyCallBackForChangeSet('rebalanceForSalesChannelGroup', $records);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param string $callback function
+     * @param array $changeSet
+     * @throws \Exception
+     */
+    protected function applyCallBackForChangeSet($callback, array $changeSet)
+    {
+        try {
+            array_walk($changeSet, [$this, $callback]);
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
         }
     }
 
@@ -74,11 +94,34 @@ class SalesChannelGroupInventoryRebalanceListener
         return ($entity instanceof SalesChannelGroup);
     }
 
+    /**
+     * @deprecated since 2.1.1 will be removed in 3.0 use rebalanceForSalesChannelGroup(SalesChannelGroup $entity) instead
+     * @throws \Oro\Component\MessageQueue\Transport\Exception\Exception
+     */
     protected function triggerRebalance()
     {
         $this->messageProducer->send(
             Topics::RESOLVE_REBALANCE_ALL_INVENTORY,
             Topics::ALL_INVENTORY
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     * @param SalesChannelGroup $entity
+     * @throws \Oro\Component\MessageQueue\Transport\Exception\Exception
+     */
+    protected function rebalanceForSalesChannelGroup(SalesChannelGroup $entity)
+    {
+        foreach ($entity->getSalesChannels() as $salesChannel) {
+            $products = $this->em->getRepository(Product::class)->findByChannel($salesChannel);
+            /** @var Product $product */
+            foreach ($products as $product) {
+                $this->messageProducer->send(
+                    Topics::RESOLVE_REBALANCE_INVENTORY,
+                    ['product_id' => $product->getId(), 'jobId' => md5($product->getId())]
+                );
+            }
+        }
     }
 }
