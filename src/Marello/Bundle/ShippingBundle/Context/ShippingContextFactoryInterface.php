35cc752369708968102bf72baa6fbba04b6de300
Hotlander <j.casteleijn@labelgoods.nl>
2019-09-24 11:11:07 +0200

    - Reverted changes that break BC, added comments where BC would break if we would keep the changes in the Interface - Added alternatives to provide same functionality but without BC breaks

--- src/Marello/Bundle/ShippingBundle/Context/ShippingContextFactoryInterface.php
+++ src/Marello/Bundle/ShippingBundle/Context/ShippingContextFactoryInterface.php
@@ -4,16 +4,13 @@ namespace Marello\Bundle\ShippingBundle\Context;
 
 interface ShippingContextFactoryInterface
 {
+    // estimation method (public function setEstimation($estimation = false)
+    // will be included in 3.0, not in 2.2 because of BC breaks
+
     /**
      * @param object $entity
      *
      * @return ShippingContextInterface[]
      */
     public function create($entity);
-
-    /**
-     * @param bool $estimation
-     * @return $this
-     */
-    public function setEstimation($estimation = false);
 }
