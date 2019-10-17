9a70104697a8b2d29ef548eb2ffd0e82d62005c9
Volodymyr Denchyk <devit_dvv@ukr.net>
2019-10-17 13:54:34 +0300

    fixed bug in Migrations versions

--- src/Marello/Bundle/SalesBundle/Migrations/Schema/v1_2/MarelloSalesBundle.php
+++ src/Marello/Bundle/SalesBundle/Migrations/Schema/v1_2/MarelloSalesBundle.php
@@ -3,10 +3,6 @@
 namespace Marello\Bundle\SalesBundle\Migrations\Schema\v1_2;
 
 use Doctrine\DBAL\Schema\Schema;
-
-use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
-use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtension;
-use Oro\Bundle\EntityExtendBundle\Migration\Extension\ExtendExtensionAwareInterface;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
 
