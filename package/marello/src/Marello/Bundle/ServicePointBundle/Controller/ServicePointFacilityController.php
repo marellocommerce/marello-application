098d8a3c09bfac301dac63cf16147e1f5d0fd198
Geert Lamers <geert@madia.nl>
2019-10-29 17:21:05 +0100

    CRH19001-18; Add ACL

--- package/marello/src/Marello/Bundle/ServicePointBundle/Controller/ServicePointFacilityController.php
+++ package/marello/src/Marello/Bundle/ServicePointBundle/Controller/ServicePointFacilityController.php
@@ -4,6 +4,8 @@ namespace Marello\Bundle\ServicePointBundle\Controller;
 
 use Marello\Bundle\ServicePointBundle\Entity\ServicePoint;
 use Marello\Bundle\ServicePointBundle\Entity\ServicePointFacility;
+use Oro\Bundle\SecurityBundle\Annotation\Acl;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -16,6 +18,12 @@ class ServicePointFacilityController extends Controller
      * @Route("/create/{servicepoint_id}", requirements={"servicepoint_id"="\d+"}, name="marello_servicepoint_servicepointfacility_create")
      * @ParamConverter("servicePoint", class="Marello\Bundle\ServicePointBundle\Entity\ServicePoint", options={"id" = "servicepoint_id"})
      * @Template("MarelloServicePointBundle:ServicePointFacility:update.html.twig")
+     * @Acl(
+     *      id="marello_servicepoint_servicepointfacility_create",
+     *      type="entity",
+     *      class="MarelloServicePointBundle:ServicePointFacility",
+     *      permission="CREATE"
+     * )
      */
     public function createAction(Request $request, ServicePoint $servicePoint)
     {
@@ -28,6 +36,12 @@ class ServicePointFacilityController extends Controller
     /**
      * @Route("/update/{id}", requirements={"id" = "\d+"}, name="marello_servicepoint_servicepointfacility_update")
      * @Template("MarelloServicePointBundle:ServicePointFacility:update.html.twig")
+     * @Acl(
+     *      id="marello_servicepoint_servicepointfacility_update",
+     *      type="entity",
+     *      class="MarelloServicePointBundle:ServicePointFacility",
+     *      permission="EDIT"
+     * )
      */
     public function updateAction(Request $request, ServicePointFacility $entity)
     {
@@ -37,6 +51,12 @@ class ServicePointFacilityController extends Controller
     /**
      * @Route("/view/{id}", requirements={"id" = "\d+"}, name="marello_servicepoint_servicepointfacility_view")
      * @Template
+     * @Acl(
+     *      id="marello_servicepoint_servicepointfacility_view",
+     *      type="entity",
+     *      class="MarelloServicePointBundle:ServicePointFacility",
+     *      permission="VIEW"
+     * )
      */
     public function viewAction(Request $request, ServicePointFacility $entity)
     {
@@ -46,6 +66,7 @@ class ServicePointFacilityController extends Controller
     /**
      * @Route("/businesshours/{id}", requirements={"id" = "\d+"}, name="marello_servicepoint_servicepointfacility_businesshours")
      * @Template("MarelloServicePointBundle:ServicePointFacility:widget/businessHours.html.twig")
+     * @AclAncestor("marello_servicepoint_servicepointfacility_view")
      */
     public function businesHoursAction(Request $request, ServicePointFacility $entity)
     {
