6bb210c740a98ea47ca1e5337b8fd3ec22a3d86c
Volodymyr Denchyk <devit_dvv@ukr.net>
2019-11-07 20:52:32 +0200

    MAR10001-759: Update OroCommerce bridge for Marello 2.2

--- src/Marello/Bundle/InventoryBundle/Tests/Functional/Controller/InventoryControllerTest.php
+++ src/Marello/Bundle/InventoryBundle/Tests/Functional/Controller/InventoryControllerTest.php
@@ -49,7 +49,7 @@ class InventoryControllerTest extends WebTestCase
             $this->getUrl(
                 'marello_inventory_inventory_view',
                 [
-                    'id' => $this->getReference(LoadProductData::PRODUCT_1_REF)
+                    'id' => $this->getReference(LoadProductData::PRODUCT_1_REF)->getId()
                 ]
             )
         );
@@ -67,7 +67,7 @@ class InventoryControllerTest extends WebTestCase
             $this->getUrl(
                 'marello_inventory_inventory_update',
                 [
-                    'id' => $this->getReference(LoadProductData::PRODUCT_1_REF)
+                    'id' => $this->getReference(LoadProductData::PRODUCT_1_REF)->getId()
                 ]
             )
         );
