1f47903e24c439c04274e82ac061b579c9a038f3
Hotlander <j.casteleijn@labelgoods.nl>
2019-11-07 17:04:44 +0100

    - CS fixes - Changed php version back to 7.1

--- package/marello/src/Marello/Bundle/InventoryBundle/Validator/InventoryOrderOnDemandValidator.php
+++ package/marello/src/Marello/Bundle/InventoryBundle/Validator/InventoryOrderOnDemandValidator.php
@@ -29,12 +29,18 @@ class InventoryOrderOnDemandValidator extends ConstraintValidator
                 ->addViolation();
         }
         if ($entity->isOrderOnDemandAllowed() && $entity->isBackorderAllowed()) {
-            $this->context->buildViolation('marello.inventory.validation.messages.error.inventoryitem.order_on_demand_allowed_and_backorder_allowed')
+            $this->context
+                ->buildViolation(
+                    'marello.inventory.validation.messages.error.inventoryitem.order_on_demand_not_allowed_backorder'
+                )
                 ->atPath('orderOnDemandAllowed')
                 ->addViolation();
         }
         if ($entity->isOrderOnDemandAllowed() && $entity->isCanPreorder()) {
-            $this->context->buildViolation('marello.inventory.validation.messages.error.inventoryitem.order_on_demand_allowed_and_preorder_allowed')
+            $this->context
+                ->buildViolation(
+                    'marello.inventory.validation.messages.error.inventoryitem.order_on_demand_not_allowed_preorder'
+                )
                 ->atPath('orderOnDemandAllowed')
                 ->addViolation();
         }
