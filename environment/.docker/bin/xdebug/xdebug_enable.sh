369323d8f3c95577f71c4efbc8fb581f0814375a
Hotlander <j.casteleijn@labelgoods.nl>
2019-11-07 11:22:44 +0100

    - updated oro platform version to 3.1.15 - added script to enable / disable xdebug, can only be used when logged in as root in the container

---
+++ environment/.docker/bin/xdebug/xdebug_enable.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sed -i -e "s/;\+zend_extension/zend_extension/g" /etc/php/7.1/cli/conf.d/20-xdebug.ini
+sed -i -e "s/;\+zend_extension/zend_extension/g" /etc/php/7.1/fpm/conf.d/20-xdebug.ini
+supervisorctl restart php-fpm
