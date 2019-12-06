18843e469c23dfe4711470f3df266baa13567152
Volodymyr Denchyk <devit_dvv@ukr.net>
2019-12-06 10:55:07 +0200

    MAR10001-765: Update InventoryRebalance Command

--- src/MarelloEnterprise/Bundle/InstoreAssistantBundle/Controller/Api/CorsOptionsController.php
+++ src/MarelloEnterprise/Bundle/InstoreAssistantBundle/Controller/Api/CorsOptionsController.php
@@ -4,11 +4,29 @@ namespace MarelloEnterprise\Bundle\InstoreAssistantBundle\Controller\Api;
 
 use MarelloEnterprise\Bundle\InstoreAssistantBundle\Api\Handler\InstoreAssistantRequestActionHandler;
 use Oro\Bundle\ApiBundle\Controller\RestApiController;
+use Oro\Bundle\ApiBundle\Request\Rest\RequestHandler;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class CorsOptionsController extends RestApiController
 {
+    /**
+     * @var InstoreAssistantRequestActionHandler
+     */
+    private $requestActionHandler;
+
+    /**
+     * @param RequestHandler $requestHandler
+     * @param InstoreAssistantRequestActionHandler $requestActionHandler
+     */
+    public function __construct(
+        RequestHandler $requestHandler,
+        InstoreAssistantRequestActionHandler $requestActionHandler
+    ) {
+        parent::__construct($requestHandler);
+        $this->requestActionHandler = $requestActionHandler;
+    }
+    
     /**
      * {@inheritdoc}
      * @param Request $request
@@ -17,14 +35,6 @@ class CorsOptionsController extends RestApiController
      */
     public function optionsAction(Request $request)
     {
-        return $this->getHandler()->handleOptions($request);
-    }
-
-    /**
-     * @return InstoreAssistantRequestActionHandler
-     */
-    private function getHandler(): InstoreAssistantRequestActionHandler
-    {
-        return $this->get('marelloenterprise_instoreassistant.api.handler.request_action_handler');
+        return $this->requestActionHandler->handleOptionsList($request);
     }
 }
