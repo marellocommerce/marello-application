16afee105722e29e995a8dc0770b4c2735d61293
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-01-21 18:54:11 +0200

    MAR10001-776: Inventory Batches - InventoryBatches combined in replenishment orders

--- package/marello-enterprise/src/MarelloEnterprise/Bundle/ReplenishmentBundle/EventListener/Doctrine/ReplenishmentOrderItemListener.php
+++ package/marello-enterprise/src/MarelloEnterprise/Bundle/ReplenishmentBundle/EventListener/Doctrine/ReplenishmentOrderItemListener.php
@@ -3,6 +3,8 @@
 namespace MarelloEnterprise\Bundle\ReplenishmentBundle\EventListener\Doctrine;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Marello\Bundle\InventoryBundle\Entity\InventoryBatch;
+use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
 use Marello\Bundle\InventoryBundle\Event\InventoryUpdateEvent;
 use Marello\Bundle\InventoryBundle\Model\InventoryUpdateContextFactory;
 use MarelloEnterprise\Bundle\ReplenishmentBundle\Entity\ReplenishmentOrderItem;
@@ -45,6 +47,42 @@ class ReplenishmentOrderItemListener
             $changes['inventoryQty'][0] !== null &&
             $changes['inventoryQty'][0] !== $changes['inventoryQty'][1]) {
             $diff = $changes['inventoryQty'][1] - $changes['inventoryQty'][0];
+            $product = $item->getProduct();
+            $origin = $item->getOrder()->getOrigin();
+            if ($product) {
+                /** @var InventoryItem $inventoryItem */
+                $inventoryItem = $product->getInventoryItems()->first();
+                if ($inventoryItem && $inventoryItem->isEnableBatchInventory()) {
+                    if ($inventoryLevel = $inventoryItem->getInventoryLevel($origin)) {
+                        /** @var InventoryBatch[] $inventoryBatches */
+                        $inventoryBatches = $inventoryLevel->getInventoryBatches()->toArray();
+                        if (count($inventoryBatches) > 0) {
+                            usort($inventoryBatches, function (InventoryBatch $a, InventoryBatch $b) {
+                                if ($a->getDeliveryDate() < $b->getDeliveryDate()) {
+                                    return -1;
+                                } elseif ($a->getDeliveryDate() > $b->getDeliveryDate()) {
+                                    return 1;
+                                } else {
+                                    return 0;
+                                }
+                            });
+                            $data = [];
+                            $quantity = $changes['inventoryQty'][1];
+                            /** @var InventoryBatch[] $inventoryBatches */
+                            foreach ($inventoryBatches as $inventoryBatch) {
+                                if ($inventoryBatch->getQuantity() >= $quantity) {
+                                    $data[$inventoryBatch->getBatchNumber()] = $quantity;
+                                    break;
+                                } elseif ($batchQty = $inventoryBatch->getQuantity() > 0) {
+                                    $data[$inventoryBatch->getBatchNumber()] = $batchQty;
+                                    $quantity = $quantity - $batchQty;
+                                }
+                            }
+                            $item->setInventoryBatches($data);
+                        }
+                    }
+                }
+            }
             $context = InventoryUpdateContextFactory::createInventoryUpdateContext(
                 $item,
                 null,
