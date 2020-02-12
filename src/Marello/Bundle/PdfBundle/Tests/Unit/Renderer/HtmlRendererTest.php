d12c41ce2b587ea5903dad127cf6206582542768
Hotlander <j.casteleijn@labelgoods.nl>
2020-02-12 16:08:33 +0100

    - Merged improvements for PDF capabilities - Updated entities with SalesChannelAwareInterface who were already had a 'getSalesChannel()' method available - Renamed the ChannelAwareInterface to SalesChannelAwareInterface for clarity and consistency - Renamed the old SalesChannelAwareInterface to SalesChannelsAwareInterface to indicate the differences in interfaces, this one will have functions to add, remove and get multiple SalesChannels instead of a single SalesChannel

--- src/Marello/Bundle/PdfBundle/Tests/Unit/Renderer/HtmlRendererTest.php
+++ src/Marello/Bundle/PdfBundle/Tests/Unit/Renderer/HtmlRendererTest.php
@@ -44,7 +44,6 @@ class HtmlRendererTest extends TestCase
     public function testRender($input, $expected)
     {
         $generatedPdf = $this->renderer->render($input);
-        $this->assertEquals($expected, $generatedPdf);
         $expectedMagick = new \Imagick();
         $expectedMagick->readImageBlob($expected);
         $expectedMagick->resetIterator();
