24df47beb9c473d5b479ddebbbc70a0cba6f399d
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-03-04 12:50:38 +0200

    MAR10001-791: Marello Payments

--- environment/.docker/bin/cron.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-/usr/local/bin/waitinstall.sh
-#exec /usr/local/bin/listener.php /var/log/oro-cron.log /var/www/app/console --env=prod oro:cron
-exec /usr/local/bin/listener.php /var/www/logs/oro-cron.log /sbin/runuser -s /bin/sh -c "exec /usr/bin/php /var/www/bin/console oro:cron" www-data
--- package/marello/src/Marello/Bundle/CoreBundle/Validator/Exception/InvalidMethodException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Marello\Bundle\CoreBundle\Validator\Exception;
-
-use Symfony\Component\Validator\Exception\ValidatorException;
-
-class InvalidMethodException extends ValidatorException
-{
-}
