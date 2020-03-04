461d1146849ca633209b39dec12090b4e9310ea2
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-03-04 12:50:38 +0200

    MAR10001-791: Marello Payments

--- package/marello/src/Marello/Bundle/OrderBundle/Model/WorkflowNameProviderInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Marello\Bundle\OrderBundle\Model;
-
-interface WorkflowNameProviderInterface
-{
-    const ORDER_WORKFLOW_1 = 'marello_order_b2c_workflow_1';
-    const ORDER_WORKFLOW_2 = 'marello_order_b2c_workflow_2';
-}
