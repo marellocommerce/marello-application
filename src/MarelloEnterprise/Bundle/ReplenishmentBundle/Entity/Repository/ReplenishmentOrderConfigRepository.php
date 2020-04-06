0e13a8944fbde5e082ea5de585a4d6d1e8a535ce
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-04-06 21:40:14 +0300

    MAR10001-791: Marello Payments - fixed functional tests - fixed Invoice totalPaid, totalDue calculations

--- src/MarelloEnterprise/Bundle/ReplenishmentBundle/Entity/Repository/ReplenishmentOrderConfigRepository.php
+++ src/MarelloEnterprise/Bundle/ReplenishmentBundle/Entity/Repository/ReplenishmentOrderConfigRepository.php
@@ -3,20 +3,7 @@
 namespace MarelloEnterprise\Bundle\ReplenishmentBundle\Entity\Repository;
 
 use Doctrine\ORM\EntityRepository;
-use Oro\Bundle\SecurityBundle\ORM\Walker\AclHelper;
 
 class ReplenishmentOrderConfigRepository extends EntityRepository
 {
-    /**
-     * @var AclHelper
-     */
-    private $aclHelper;
-
-    /**
-     * @param AclHelper $aclHelper
-     */
-    public function setAclHelper(AclHelper $aclHelper)
-    {
-        $this->aclHelper = $aclHelper;
-    }
 }
