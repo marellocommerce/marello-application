98d09368909f14ed22ae6fd83c65aee25fb123a1
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-05-06 11:46:41 +0300

    MAR10001-879: Verify best possible approach for updating multiple products with multiple consumers in OroCommerce Bridge

--- src/Marello/Bridge/MarelloOroCommerceApi/MarelloOroCommerceApiBridgeBundle.php
+++ src/Marello/Bridge/MarelloOroCommerceApi/MarelloOroCommerceApiBridgeBundle.php
@@ -2,8 +2,19 @@
 
 namespace Marello\Bridge\MarelloOroCommerceApi;
 
+use Marello\Bridge\MarelloOroCommerceApi\DependencyInjection\CompilerPass\AddOroProcessorsCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MarelloOroCommerceApiBridgeBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new AddOroProcessorsCompilerPass());
+    }
 }
