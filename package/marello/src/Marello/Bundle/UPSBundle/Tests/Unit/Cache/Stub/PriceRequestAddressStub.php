d09fdc485f2d068b57e26cef0cc5ef00cf8286c4
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-05-13 16:22:01 +0300

    MAR10001-911: added migrations

--- package/marello/src/Marello/Bundle/UPSBundle/Tests/Unit/Cache/Stub/PriceRequestAddressStub.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Marello\Bundle\UPSBundle\Tests\Unit\Cache\Stub;
-
-use Marello\Bundle\AddressBundle\Entity\MarelloAddress;
-
-class PriceRequestAddressStub extends MarelloAddress
-{
-}
