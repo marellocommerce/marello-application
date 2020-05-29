eadd8635726076c7083f4399d7e684e0f980f543
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-05-29 16:57:43 +0300

    MAR10001-920: Payments Update

---
+++ src/Marello/Bundle/PaymentBundle/Controller/PaymentAjaxController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Marello\Bundle\PaymentBundle\Controller;
+
+use Marello\Bundle\LayoutBundle\Context\FormChangeContext;
+use Marello\Bundle\PaymentBundle\Entity\Payment;
+use Marello\Bundle\PaymentBundle\Form\Type\PaymentCreateType;
+use Oro\Bundle\SecurityBundle\Annotation\AclAncestor;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PaymentAjaxController extends AbstractController
+{
+    /**
+     * @Route(
+     *     path="/form-changes/{id}",
+     *     methods={"POST"},
+     *     name="marello_payment_form_changes",
+     *     defaults={"id" = 0}
+     * )
+     * @AclAncestor("marello_payment_create")
+     *
+     * @param Request $request
+     * @param Payment|null $payment
+     * @return JsonResponse
+     */
+    public function formChangesAction(Request $request, Payment $payment = null)
+    {
+        if (!$payment) {
+            $payment = new Payment();
+        }
+
+        $form = $this->getType($payment);
+        $submittedData = $request->get($form->getName());
+
+        $form->submit($submittedData);
+
+        $context = new FormChangeContext(
+            [
+                FormChangeContext::FORM_FIELD => $form,
+                FormChangeContext::SUBMITTED_DATA_FIELD => $submittedData,
+                FormChangeContext::RESULT_FIELD => [],
+            ]
+        );
+
+        $formChangesProvider = $this->get('marello_layout.provider.form_changes_data.composite');
+        $formChangesProvider
+            ->setRequiredDataClass(Payment::class)
+            ->setRequiredFields($request->get('updateFields', []))
+            ->processFormChanges($context);
+
+        return new JsonResponse($context->getResult());
+    }
+
+    /**
+     * @param Payment $payment
+     * @return FormInterface
+     */
+    protected function getType(Payment $payment)
+    {
+        return $this->createForm(PaymentCreateType::class, $payment);
+    }
+}
