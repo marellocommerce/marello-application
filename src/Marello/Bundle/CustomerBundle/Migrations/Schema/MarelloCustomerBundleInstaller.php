259c9d70c0fc53379f3fae57f82ce92ee86c2661
Volodymyr Denchyk <devit_dvv@ukr.net>
2020-06-11 00:19:26 +0300

    MAR10001-909: Add Company ID to Company

--- src/Marello/Bundle/CustomerBundle/Migrations/Schema/MarelloCustomerBundleInstaller.php
+++ src/Marello/Bundle/CustomerBundle/Migrations/Schema/MarelloCustomerBundleInstaller.php
@@ -34,7 +34,7 @@ class MarelloCustomerBundleInstaller implements
      */
     public function getMigrationVersion()
     {
-        return 'v1_3';
+        return 'v1_4';
     }
 
     /**
@@ -63,6 +63,7 @@ class MarelloCustomerBundleInstaller implements
 
         $table->addColumn('id', 'integer', ['autoincrement' => true]);
         $table->addColumn('name', 'string', ['length' => 255]);
+        $table->addColumn('code', 'string', ['length' => 255, 'notnull' => false]);
         $table->addColumn('payment_term_id', 'integer', ['notnull' => false]);
         $table->addColumn('parent_id', 'integer', ['notnull' => false]);
         $table->addColumn('organization_id', 'integer', ['notnull' => false]);
