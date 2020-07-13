a2edc86b6385e5d6968166b12859c5bac4b1a2a0
Hotlander <j.casteleijn@labelgoods.nl>
2020-07-13 17:22:05 +0200

    - added Order status to Order which will serve as preparation for the next step in the Order Management - currently no critical parts are relying on the new Order status as of yet, it's also not visibile in the current implementation

---
+++ src/Marello/Bundle/OrderBundle/EventListener/Doctrine/PendingOrderStatusListener.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Marello\Bundle\OrderBundle\EventListener\Doctrine;
+
+use Doctrine\ORM\Event\LifecycleEventArgs;
+
+use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Bundle\EntityExtendBundle\Entity\Repository\EnumValueRepository;
+
+use Marello\Bundle\OrderBundle\Entity\Order;
+use Marello\Bundle\OrderBundle\Model\OrderStatusesInterface;
+
+class PendingOrderStatusListener
+{
+    /**
+     * @var DoctrineHelper
+     */
+    protected $doctrineHelper;
+
+    /**
+     * @param DoctrineHelper $doctrineHelper
+     */
+    public function __construct(DoctrineHelper $doctrineHelper)
+    {
+        $this->doctrineHelper = $doctrineHelper;
+    }
+
+    /**
+     * @param LifecycleEventArgs $args
+     */
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if ($entity instanceof Order && $entity->getOrderStatus() === null) {
+            $className = ExtendHelper::buildEnumValueClassName(OrderStatusesInterface::ORDER_STATUS_ENUM_CLASS);
+            /** @var EnumValueRepository $enumRepo */
+            $enumRepo = $this->doctrineHelper
+                ->getEntityManagerForClass($className)
+                ->getRepository($className);
+
+            $orderStatus = $enumRepo->findOneBy(['id' => 'pending']);
+            $entity->setOrderStatus($orderStatus);
+        }
+    }
+}
