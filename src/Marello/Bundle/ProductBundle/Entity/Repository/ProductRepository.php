361a7651a19a1b7edf2f0c3d86b38fdc9a02a72c
Hotlander <j.casteleijn@labelgoods.nl>
2020-07-28 12:25:48 +0200

    - Updated email templates with new style

--- src/Marello/Bundle/ProductBundle/Entity/Repository/ProductRepository.php
+++ src/Marello/Bundle/ProductBundle/Entity/Repository/ProductRepository.php
@@ -210,7 +210,7 @@ class ProductRepository extends EntityRepository
             ->where("sup.name <> ''")
             ->andWhere("s.name = 'enabled'")
             ->andWhere("i.replenishment = 'never_out_of_stock'")
-            ->groupBy('p.sku, sup.name')
+            ->groupBy('p.sku, sup.name, i.desiredInventory, i.purchaseInventory')
             ->having('SUM(l.inventory - l.allocatedInventory) < i.purchaseInventory');
 
         return $this->aclHelper->apply($qb->getQuery())->getResult();
