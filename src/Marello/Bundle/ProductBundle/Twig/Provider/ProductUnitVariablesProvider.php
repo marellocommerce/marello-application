0a23414cc083a98dbaafc9c49f4b10286caf30fc
Hotlander <j.casteleijn@labelgoods.nl>
2020-07-31 09:02:47 +0200

    - added ProductUnit variable provider for rendering the product unit in the email templates as this is an enum field which is behaving as an extended entity with a proxy class that wasn't allowed in the email templates

---
+++ src/Marello/Bundle/ProductBundle/Twig/Provider/ProductUnitVariablesProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Marello\Bundle\ProductBundle\Twig\Provider;
+
+use Proxies\__CG__\Extend\Entity\EV_Marello_Product_Unit;
+use Oro\Bundle\EntityBundle\Twig\Sandbox\EntityVariablesProviderInterface;
+
+class ProductUnitVariablesProvider implements EntityVariablesProviderInterface
+{
+    /**
+     * @inheritDoc
+     */
+    public function getVariableDefinitions(): array
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getVariableGetters(): array
+    {
+        return [
+            EV_Marello_Product_Unit::class => [
+                'name' => 'getName',
+                'id' => 'getId'
+            ]
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getVariableProcessors(string $entityClass): array
+    {
+        return [];
+    }
+}
