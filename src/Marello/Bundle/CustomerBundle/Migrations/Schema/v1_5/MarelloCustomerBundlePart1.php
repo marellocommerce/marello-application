89284c7368d930d27f8ceb516efc5232a4748eda
Hotlander <j.casteleijn@labelgoods.nl>
2021-02-05 15:29:31 +0100

    - Added additional check to see which query needs to be running for updating tax_identification_number based on db driver

--- src/Marello/Bundle/CustomerBundle/Migrations/Schema/v1_5/MarelloCustomerBundlePart1.php
+++ src/Marello/Bundle/CustomerBundle/Migrations/Schema/v1_5/MarelloCustomerBundlePart1.php
@@ -2,18 +2,22 @@
 
 namespace Marello\Bundle\CustomerBundle\Migrations\Schema\v1_5;
 
+use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Schema\Schema;
 
+use Oro\Bundle\EntityBundle\ORM\DatabaseDriverInterface;
 use Oro\Bundle\MigrationBundle\Migration\Migration;
 use Oro\Bundle\MigrationBundle\Migration\OrderedMigrationInterface;
 use Oro\Bundle\MigrationBundle\Migration\QueryBag;
-use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtension;
-use Oro\Bundle\ActivityBundle\Migration\Extension\ActivityExtensionAwareInterface;
+use Oro\Bundle\MigrationBundle\Migration\ConnectionAwareInterface;
 
 use Marello\Bundle\CustomerBundle\Migrations\Schema\MarelloCustomerBundleInstaller;
 
-class MarelloCustomerBundlePart1 implements Migration, OrderedMigrationInterface
+class MarelloCustomerBundlePart1 implements Migration, OrderedMigrationInterface, ConnectionAwareInterface
 {
+    /** @var Connection */
+    private $connection;
+
     /**
      * @inheritDoc
      */
@@ -29,10 +33,27 @@ class MarelloCustomerBundlePart1 implements Migration, OrderedMigrationInterface
     {
         $table = $schema->getTable(MarelloCustomerBundleInstaller::MARELLO_COMPANY_TABLE);
         $table->addColumn('tax_identification_number', 'string', ['notnull' => false, 'length' => 255]);
-        $sql = <<<EOF
+        if ($this->connection->getDriver()->getName() === DatabaseDriverInterface::DRIVER_POSTGRESQL) {
+            $sql = <<<EOF
 UPDATE marello_customer_company AS co SET tax_identification_number = cu.tax_identification_number FROM marello_customer_customer AS cu 
 WHERE co.id = cu.company_id AND cu.tax_identification_number IS NOT NULL
 EOF;
+        } else {
+            $sql = <<<EOF
+UPDATE marello_customer_company co INNER JOIN marello_customer_customer cu ON co.id = cu.company_id SET co.tax_identification_number = cu.tax_identification_number
+WHERE cu.tax_identification_number IS NOT NULL
+EOF;
+        }
+
         $queries->addPostQuery($sql);
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function setConnection(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
 }
