695e5246e82b5d86ac93347317979e4c33fcdcca
Hotlander <j.casteleijn@labelgoods.nl>
2021-12-27 11:42:51 +0100

    - Upgrade to Oro 4.2.7 - Fixes for PHP 8.0.x - Added fix for exposing fos js routes in marello enterprise application - Updated Dockerfile && docker-compose-ee.yml file for upgrade

--- src/Marello/Bundle/ReturnBundle/Workflow/InspectionAction.php
+++ src/Marello/Bundle/ReturnBundle/Workflow/InspectionAction.php
@@ -55,7 +55,7 @@ class InspectionAction extends AbstractAction
 
         $return->getReturnItems()->map(function (ReturnItem $item) use ($return) {
             if (!$item->getReason() || !$item->getStatus()) {
-                throw new \Exception($this->translator->trans('marello.return.returnentity.messages.error.workflow.consumer'));
+                throw new \Exception($this->translator->trans('marello.return.returnentity.messages.error.return.workflow.consumer'));
             }
             if (($item->getReason()->getId() !== 'damaged') && ($item->getStatus()->getId() !== 'denied')) {
                 $this->handleInventoryUpdate(
