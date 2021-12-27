8f588d58e7cd26934f0b4a2af4734c101abbd361
Viacheslav Bemza <bemzaslava@gmail.com>
2021-12-27 14:27:45 +0200

    MAR10001-1006: Update platform to Oro BAP 4.2 - fix unit tests according to new phpunit version - revert TestListener to a phpunit.xml.dist

--- src/MarelloEnterprise/Bundle/InventoryBundle/Tests/Unit/Strategy/MinimumQuantity/Calculator/Chain/Element/SingleWarehouse/SingleWHCalculatorChainElementTest.php
+++ src/MarelloEnterprise/Bundle/InventoryBundle/Tests/Unit/Strategy/MinimumQuantity/Calculator/Chain/Element/SingleWarehouse/SingleWHCalculatorChainElementTest.php
@@ -3,17 +3,15 @@
 // @codingStandardsIgnoreStart
 namespace MarelloEnterprise\Bundle\InventoryBundle\Tests\Unit\Strategy\MinimumQuantity\Calculator\Chain\Element\SingleWarehouse;
 
-// @codingStandardsIgnoreEnd
-
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Marello\Bundle\InventoryBundle\Entity\Warehouse;
 use Marello\Bundle\InventoryBundle\Model\OrderWarehouseResult;
 use Marello\Bundle\OrderBundle\Entity\OrderItem;
-use MarelloEnterprise\Bundle\InventoryBundle\Strategy\MinimumQuantity\Calculator\Chain\Element\SingleWarehouse\
-SingleWHCalculatorChainElement;
-use MarelloEnterprise\Bundle\InventoryBundle\Tests\Unit\Strategy\MinimumQuantity\Calculator\Chain\Element\
-AbstractWHCalculatorChainElementTest;
+use MarelloEnterprise\Bundle\InventoryBundle\Strategy\MinimumQuantity\Calculator\Chain\Element\SingleWarehouse\SingleWHCalculatorChainElement;
+use MarelloEnterprise\Bundle\InventoryBundle\Tests\Unit\Strategy\MinimumQuantity\Calculator\Chain\Element\AbstractWHCalculatorChainElementTest;
+
+// @codingStandardsIgnoreEnd
 
 class SingleWHCalculatorChainElementTest extends AbstractWHCalculatorChainElementTest
 {
