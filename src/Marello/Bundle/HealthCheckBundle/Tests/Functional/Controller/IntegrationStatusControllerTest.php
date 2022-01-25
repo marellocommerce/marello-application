3ceab748a7447eb18663b4c202a76129f96c3d0c
Viacheslav Bemza <bemzaslava@gmail.com>
2022-01-25 13:44:24 +0200

    MAR10001-1016: Simple Integration Status monitor - added MarelloHealthCheckBundle - created grid + dashboard widget

---
+++ src/Marello/Bundle/HealthCheckBundle/Tests/Functional/Controller/IntegrationStatusControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Marello\Bundle\InventoryBundle\Tests\Functional\Controller;
+
+use Oro\Bundle\IntegrationBundle\Tests\Functional\DataFixtures\LoadStatusData;
+use Oro\Bundle\TestFrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class IntegrationStatusControllerTest extends WebTestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp(): void
+    {
+        $this->initClient(
+            [],
+            $this->generateBasicAuthHeader()
+        );
+
+        $this->loadFixtures([
+            LoadStatusData::class,
+        ]);
+    }
+
+    public function testIndexAction()
+    {
+        $this->client->request(
+            'GET',
+            $this->getUrl(
+                'marello_healthcheck_integration_statuses_index',
+                []
+            )
+        );
+
+        $this->assertResponseStatusCodeEquals($this->client->getResponse(), Response::HTTP_OK);
+    }
+}
