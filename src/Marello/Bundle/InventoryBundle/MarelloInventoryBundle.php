01da579e1b4f516109ca52311148edaba6c13bd1
Hotlander <j.casteleijn@labelgoods.nl>
2022-04-22 10:54:14 +0200

    feature/MAR10001-0-order-allocation: - Removed packingslip creation in Order Workflow - Moved WFA strategy registration to CE version - Added default strategy in CE - Renamed directories and interfaces for clarity - Added services to support WFA strategy registration - Removed references from EE on WFA strategy registration - Updated Allocation navigation item and labels for pages - Added sorting mechanism with configuration for Mixed, Internal and External warehouse sorting

--- src/Marello/Bundle/InventoryBundle/MarelloInventoryBundle.php
+++ src/Marello/Bundle/InventoryBundle/MarelloInventoryBundle.php
@@ -5,6 +5,7 @@ namespace Marello\Bundle\InventoryBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
+use Marello\Bundle\InventoryBundle\DependencyInjection\CompilerPass\WFAStrategiesCompilerPass;
 use Marello\Bundle\InventoryBundle\DependencyInjection\CompilerPass\BalancerStrategiesCompilerPass;
 
 class MarelloInventoryBundle extends Bundle
@@ -15,6 +16,7 @@ class MarelloInventoryBundle extends Bundle
     public function build(ContainerBuilder $container)
     {
         $container->addCompilerPass(new BalancerStrategiesCompilerPass());
+        $container->addCompilerPass(new WFAStrategiesCompilerPass());
 
         parent::build($container);
     }
