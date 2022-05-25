a213d5e4b34d6b3e704579fbd214ddc5c4ed26fa
Hotlander <j.casteleijn@labelgoods.nl>
2022-05-25 11:43:22 +0200

    feature/MAR10001-0-order-allocation: - added hide/show for product supplier relation canDropship option based on the Supplier's data

--- src/Marello/Bundle/SupplierBundle/Resources/public/js/app/views/product-supplier-view.js
+++ src/Marello/Bundle/SupplierBundle/Resources/public/js/app/views/product-supplier-view.js
@@ -76,6 +76,15 @@ define(function(require) {
 
             if (data.canDropship.length !== 0) {
                 this.options.canDropship = data.canDropship;
+                if (this.fieldsByName.canDropship !== undefined) {
+                    this.fieldsByName.canDropship
+                        .prop('checked', this.options.canDropship);
+                    if (this.options.canDropship) {
+                        $(this.fieldsByName.canDropship).parent().show();
+                    } else {
+                        $(this.fieldsByName.canDropship).parent().hide();
+                    }
+                }
             }
 
             if (data.currency.length !== 0) {
@@ -84,9 +93,6 @@ define(function(require) {
 
             this.fieldsByName.priority
                 .val(this.options.priority);
-
-            this.fieldsByName.canDropship
-                .prop('checked', this.options.canDropship);
 
             var parent = $(this.fieldsByName.cost).parent();
             parent.contents().filter(function(){
