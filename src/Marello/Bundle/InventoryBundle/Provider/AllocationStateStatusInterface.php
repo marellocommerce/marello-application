65c4247aba6a04fd51f689a4563fd6a88a28d5a7
Hotlander <j.casteleijn@labelgoods.nl>
2022-06-09 16:44:41 +0200

    feature/MAR10001-0-order-allocation: - update label system setting enabling consolidation - update label system setting enabling feature - Move current waiting for supply allocation to ‘closed’ - Add closed state / status for allocation

--- src/Marello/Bundle/InventoryBundle/Provider/AllocationStateStatusInterface.php
+++ src/Marello/Bundle/InventoryBundle/Provider/AllocationStateStatusInterface.php
@@ -8,6 +8,7 @@ interface AllocationStateStatusInterface
     const ALLOCATION_STATE_AVAILABLE = 'available';
     const ALLOCATION_STATE_WFS       = 'waiting';
     const ALLOCATION_STATE_ALERT     = 'alert';
+    const ALLOCATION_STATE_CLOSED     = 'closed';
 
     const ALLOCATION_STATUS_ENUM_CODE   = 'marello_allocation_status';
     const ALLOCATION_STATUS_ON_HAND     = 'on_hand';
@@ -15,4 +16,5 @@ interface AllocationStateStatusInterface
     const ALLOCATION_STATUS_BACK_ORDER  = 'backorder';
     const ALLOCATION_STATUS_PRE_ORDER   = 'preorder';
     const ALLOCATION_STATUS_CNA         = 'could_not_allocate';
+    const ALLOCATION_STATUS_CLOSED      = 'closed';
 }
