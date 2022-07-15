939a8d8c96560992f760b1af855645b74f47e630
Hotlander <j.casteleijn@labelgoods.nl>
2022-07-15 12:20:49 +0200

    bugfix/MAR10001-1074: - fixed issue items being allocated in wrong allocation with incorrect orders

--- src/Marello/Bundle/InventoryBundle/Command/InventoryReAllocateCronCommand.php
+++ src/Marello/Bundle/InventoryBundle/Command/InventoryReAllocateCronCommand.php
@@ -7,10 +7,14 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 use Oro\Bundle\EntityBundle\ORM\DoctrineHelper;
+use Oro\Bundle\WorkflowBundle\Entity\WorkflowItem;
 use Oro\Bundle\EntityExtendBundle\Tools\ExtendHelper;
+use Oro\Component\MessageQueue\Client\MessagePriority;
 use Oro\Bundle\CronBundle\Command\CronCommandInterface;
+use Oro\Component\MessageQueue\Client\MessageProducerInterface;
 use Oro\Bundle\EntityExtendBundle\Entity\Repository\EnumValueRepository;
 
+use Marello\Bundle\WorkflowBundle\Async\Topics;
 use Marello\Bundle\InventoryBundle\Entity\Allocation;
 use Marello\Bundle\InventoryBundle\Provider\InventoryAllocationProvider;
 use Marello\Bundle\InventoryBundle\Provider\AllocationStateStatusInterface;
@@ -18,6 +22,7 @@ use Marello\Bundle\InventoryBundle\Provider\AllocationStateStatusInterface;
 class InventoryReAllocateCronCommand extends Command implements CronCommandInterface
 {
     const COMMAND_NAME = 'oro:cron:marello:inventory:reallocate';
+    const ALLOCATION_WORKFLOW_RESOLVED = 'resolved';
     const EXIT_CODE = 0;
 
     /** @var DoctrineHelper $doctrineHelper */
@@ -26,6 +31,9 @@ class InventoryReAllocateCronCommand extends Command implements CronCommandInter
     /** @var InventoryAllocationProvider $allocationProvider */
     protected $allocationProvider;
 
+    /** @var MessageProducerInterface $messageProducer */
+    protected $messageProducer;
+
     /**
      * InventoryReAllocateCronCommand constructor.
      * @param DoctrineHelper $doctrineHelper
@@ -33,12 +41,14 @@ class InventoryReAllocateCronCommand extends Command implements CronCommandInter
      */
     public function __construct(
         DoctrineHelper $doctrineHelper,
-        InventoryAllocationProvider $allocationProvider
+        InventoryAllocationProvider $allocationProvider,
+        MessageProducerInterface $messageProducer
     ) {
         parent::__construct();
         
         $this->doctrineHelper = $doctrineHelper;
         $this->allocationProvider = $allocationProvider;
+        $this->messageProducer = $messageProducer;
     }
 
     /**
@@ -56,7 +66,6 @@ class InventoryReAllocateCronCommand extends Command implements CronCommandInter
      */
     public function getDefaultDefinition()
     {
-        // tmp every 10 minutes
         return '*/10 * * * *';
     }
 
@@ -84,10 +93,11 @@ class InventoryReAllocateCronCommand extends Command implements CronCommandInter
             foreach ($this->allocationProvider->getWarehouseResults($allocation->getOrder()) as $orderWarehouseResults) {
                 foreach ($orderWarehouseResults as $result) {
                     if (!in_array($result->getWarehouse()->getCode(), ['no_warehouse', 'could_not_allocate'])) {
-                        $this->allocationProvider->allocateOrderToWarehouses($allocation->getOrder());
+                        $this->allocationProvider->allocateOrderToWarehouses($allocation->getOrder(), $allocation);
                         $allocation->setState($this->getEnumValue('marello_allocation_state', AllocationStateStatusInterface::ALLOCATION_STATE_CLOSED));
                         $allocation->setStatus($this->getEnumValue('marello_allocation_status', AllocationStateStatusInterface::ALLOCATION_STATUS_CLOSED));
                         $em->persist($allocation);
+                        $this->updateAllocationWorkflow($allocation);
                     }
                 }
             }
@@ -111,5 +121,37 @@ class InventoryReAllocateCronCommand extends Command implements CronCommandInter
             ->getEntityRepositoryForClass($className);
 
         return $enumRepo->findOneBy(['id' => $value]);
+    }
+
+    /**
+     * @param Allocation $allocation
+     * @return void
+     * @throws \Oro\Component\MessageQueue\Transport\Exception\Exception
+     */
+    protected function updateAllocationWorkflow(Allocation $allocation)
+    {
+        /** @var WorkflowItem|null $workflowItem */
+        $workflowItem = $this->doctrineHelper
+            ->getEntityRepositoryForClass(WorkflowItem::class)
+            ->findOneBy(
+                [
+                    'entityId' => $allocation->getId(),
+                    'entityClass' => Allocation::class
+                ]
+            );
+
+        if ($workflowItem) {
+            $this->messageProducer->send(
+                Topics::WORKFLOW_TRANSIT_TOPIC,
+                [
+                    'workflow_item_entity_id' => $allocation->getId(),
+                    'current_step_id' => $workflowItem->getCurrentStep()->getId(),
+                    'entity_class' => Allocation::class,
+                    'transition' => self::ALLOCATION_WORKFLOW_RESOLVED,
+                    'jobId' => md5($allocation->getId()),
+                    'priority' => MessagePriority::NORMAL
+                ]
+            );
+        }
     }
 }
