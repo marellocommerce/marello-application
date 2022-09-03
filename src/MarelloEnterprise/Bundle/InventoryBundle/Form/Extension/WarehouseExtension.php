fdbf52be4b194f77d2c92ca9944e55c3bf1166ab
Viacheslav Bemza <bemzaslava@gmail.com>
2022-09-03 20:29:13 +0300

    MAR10001-1098: Revert exclusion of External warehouse type on warehouse create/edit

--- src/MarelloEnterprise/Bundle/InventoryBundle/Form/Extension/WarehouseExtension.php
+++ src/MarelloEnterprise/Bundle/InventoryBundle/Form/Extension/WarehouseExtension.php
@@ -4,12 +4,11 @@ namespace MarelloEnterprise\Bundle\InventoryBundle\Form\Extension;
 
 use Doctrine\ORM\EntityRepository;
 
-use Marello\Bundle\InventoryBundle\Provider\WarehouseTypeProviderInterface;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-
+use Marello\Bundle\InventoryBundle\Entity\WarehouseType as WarehouseTypeEntity;
 use Marello\Bundle\InventoryBundle\Form\Type\WarehouseType;
 
 class WarehouseExtension extends AbstractTypeExtension
@@ -36,16 +35,8 @@ class WarehouseExtension extends AbstractTypeExtension
             ])
             ->add('warehouseType', EntityType::class, [
                 'label'    => 'marello.inventory.warehouse.warehouse_type.label',
-                'class'    => 'MarelloInventoryBundle:WarehouseType',
+                'class'    => WarehouseTypeEntity::class,
                 'required' => true,
-                'query_builder' => function(EntityRepository $repository) {
-                    $qb = $repository->createQueryBuilder('whtype');
-                    return $qb
-                        ->where($qb->expr()->neq('whtype.name', '?1'))
-                        ->setParameter('1', WarehouseTypeProviderInterface::WAREHOUSE_TYPE_EXTERNAL)
-                        ->orderBy('whtype.name', 'ASC')
-                        ;
-                }
             ]);
     }
 }
