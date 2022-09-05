743f4e41c8764d1b395b0318504e0d89e3d063b1
Hotlander <j.casteleijn@labelgoods.nl>
2022-09-05 09:53:56 +0200

    feature/MAR10001-1098: - added disable for warehousetype to prevent switching

--- src/MarelloEnterprise/Bundle/InventoryBundle/Form/Extension/WarehouseExtension.php
+++ src/MarelloEnterprise/Bundle/InventoryBundle/Form/Extension/WarehouseExtension.php
@@ -2,14 +2,18 @@
 
 namespace MarelloEnterprise\Bundle\InventoryBundle\Form\Extension;
 
-use Doctrine\ORM\EntityRepository;
-
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+use Oro\Bundle\FormBundle\Utils\FormUtils;
+
 use Marello\Bundle\InventoryBundle\Entity\WarehouseType as WarehouseTypeEntity;
 use Marello\Bundle\InventoryBundle\Form\Type\WarehouseType;
+use Marello\Bundle\InventoryBundle\Entity\Warehouse;
 
 class WarehouseExtension extends AbstractTypeExtension
 {
@@ -37,6 +41,24 @@ class WarehouseExtension extends AbstractTypeExtension
                 'label'    => 'marello.inventory.warehouse.warehouse_type.label',
                 'class'    => WarehouseTypeEntity::class,
                 'required' => true,
-            ]);
+            ])
+            ->addEventListener(
+                FormEvents::PRE_SET_DATA,
+                [$this, 'preSetDataListener']
+            );
+    }
+
+    /**
+     * @param FormEvent $event
+     */
+    public function preSetDataListener(FormEvent $event)
+    {
+        /** @var Warehouse $warehouse */
+        $warehouse = $event->getData();
+        $form = $event->getForm();
+
+        if ($warehouse->getWarehouseType() !== null) {
+            FormUtils::replaceField($form, 'warehouseType', ['disabled' => true]);
+        }
     }
 }
