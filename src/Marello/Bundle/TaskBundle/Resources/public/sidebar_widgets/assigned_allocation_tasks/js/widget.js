150792370a0af4a5b255dde9c4839507da261ae0
Hotlander <j.casteleijn@labelgoods.nl>
2023-01-11 17:15:17 +0100

    task/MAR10001-0-remove-task-bundle-code: - Remove custom TaskBundle

--- src/Marello/Bundle/TaskBundle/Resources/public/sidebar_widgets/assigned_allocation_tasks/js/widget.js
+++
@@ -1,8 +0,0 @@
-define(function(require) {
-    'use strict';
-
-    return {
-        ContentView: require('marellotask/js/app/views/sidebar-widget/assigned-allocation-tasks/assigned-tasks-content-view'),
-        SetupView: require('marellotask/js/app/views/sidebar-widget/assigned-allocation-tasks/assigned-tasks-setup-view')
-    };
-});
