e16470436129fe92ff2a385bd440a0918b9904ad
Viacheslav Bemza <bemzaslava@gmail.com>
2023-02-13 08:58:06 +0200

    MAR10001-1180: Add available inventory as additional column on reshipment items

--- src/Marello/Bundle/InventoryBundle/Form/Type/ReshipmentItemType.php
+++ src/Marello/Bundle/InventoryBundle/Form/Type/ReshipmentItemType.php
@@ -2,6 +2,7 @@
 
 namespace Marello\Bundle\InventoryBundle\Form\Type;
 
+use Marello\Bundle\InventoryBundle\Provider\AvailableInventoryProvider;
 use Marello\Bundle\OrderBundle\Entity\OrderItem;
 use Marello\Bundle\OrderBundle\Validator\Constraints\AvailableInventoryConstraint;
 use Symfony\Component\Form\AbstractType;
@@ -20,7 +21,9 @@ class ReshipmentItemType extends AbstractType
 
     public function __construct(
         protected TranslatorInterface $translator,
-        protected array $addQuantityErrorByForm = []
+        protected AvailableInventoryProvider $availableInventoryProvider,
+        protected array $addQuantityErrorByForm = [],
+        protected array $addAvailableQuantityErrorByForm = [],
     ) {}
 
     /**
@@ -41,6 +44,13 @@ class ReshipmentItemType extends AbstractType
                     'readonly' => true
                 ]
             ])
+            ->add('availableQuantity', NumberType::class, [
+                'label' => 'marello.order.orderitem.ordered.label',
+                'attr' => [
+                    'readonly' => true
+                ],
+                'mapped' => false,
+            ])
             ->add('quantity', NumberType::class, [
                 'label' => 'marello.order.orderitem.ordered.label',
                 'attr' => [
@@ -53,8 +63,21 @@ class ReshipmentItemType extends AbstractType
             ])
         ;
 
+        $builder->addEventListener(FormEvents::POST_SET_DATA, [$this, 'fillAvailableQuantity']);
         $builder->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'changeItemQuantity']);
         $builder->addEventListener(FormEvents::SUBMIT, [$this, 'addQuantityError']);
+    }
+
+    public function fillAvailableQuantity(FormEvent $event)
+    {
+        $form = $event->getForm();
+        /** @var OrderItem $orderItem */
+        $orderItem = $event->getData();
+        $availableInventory = $this->availableInventoryProvider->getAvailableInventory(
+            $orderItem->getProduct(),
+            $orderItem->getOrder()->getSalesChannel()
+        );
+        $form->get('availableQuantity')->setData($availableInventory);
     }
 
     public function changeItemQuantity(FormEvent $event)
@@ -62,6 +85,8 @@ class ReshipmentItemType extends AbstractType
         $data = $event->getData();
         if ($data['newQuantity'] > $data['quantity']) {
             $this->addQuantityErrorByForm[] = $event->getForm();
+        } elseif ($data['newQuantity'] > $data['availableQuantity']) {
+            $this->addAvailableQuantityErrorByForm[] = $event->getForm();
         } else {
             $data['quantity'] = $data['newQuantity'];
             $event->setData($data);
@@ -70,13 +95,27 @@ class ReshipmentItemType extends AbstractType
 
     public function addQuantityError(FormEvent $event)
     {
+        $quantityForm = $event->getForm()->get('newQuantity');
         foreach ($this->addQuantityErrorByForm as $key => $form) {
-            if ($event->getForm() === $form) {
-                $event->getForm()->get('newQuantity')->addError(new FormError(
-                    $this->translator->trans('marello.inventory.reshipment.quantity.validation_error')
-                ));
-                unset($this->addQuantityErrorByForm[$key]);
+            if ($event->getForm() !== $form) {
+                continue;
             }
+
+            $quantityForm->addError(new FormError(
+                $this->translator->trans('marello.inventory.reshipment.quantity.validation_error')
+            ));
+            unset($this->addQuantityErrorByForm[$key]);
+        }
+
+        foreach ($this->addAvailableQuantityErrorByForm as $key => $form) {
+            if ($event->getForm() !== $form) {
+                continue;
+            }
+
+            $quantityForm->addError(new FormError(
+                $this->translator->trans('marello.inventory.reshipment.available_quantity.validation_error')
+            ));
+            unset($this->addAvailableQuantityErrorByForm[$key]);
         }
     }
 
