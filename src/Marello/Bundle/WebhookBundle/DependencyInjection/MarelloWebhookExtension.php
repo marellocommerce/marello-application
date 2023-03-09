d61b33b5c566f743859ed84d8362f2dadbfd669e
oz muhsin <omar@madia.nl>
2023-03-09 14:33:29 +0100

    MAR10001-1198 Adding bundle structure

---
+++ src/Marello/Bundle/WebhookBundle/DependencyInjection/MarelloWebhookExtension.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Marello\Bundle\WebhookBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class MarelloWebhookExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config, ContainerBuilder $container)
+    {
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        /*
+        $loader->load('form.yml');
+        $loader->load('services.yml');
+        */
+    }
+}
