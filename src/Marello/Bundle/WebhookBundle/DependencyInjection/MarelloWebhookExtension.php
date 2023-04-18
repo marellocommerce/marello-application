e4f1f0958e55feec4469283382cac4d5af529266
oz muhsin <omar@madia.nl>
2023-04-18 10:36:52 +0200

    MAR10001-1203 Adding delivery count config field

--- src/Marello/Bundle/WebhookBundle/DependencyInjection/MarelloWebhookExtension.php
+++ src/Marello/Bundle/WebhookBundle/DependencyInjection/MarelloWebhookExtension.php
@@ -12,11 +12,14 @@ class MarelloWebhookExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $config, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('controllers.yml');
         $loader->load('services.yml');
         $loader->load('integration.yml');
+
+        $config = $this->processConfiguration($this->getConfiguration($configs, $container), $configs);
+        $container->prependExtensionConfig($this->getAlias(), array_intersect_key($config, array_flip(['settings'])));
     }
 }
