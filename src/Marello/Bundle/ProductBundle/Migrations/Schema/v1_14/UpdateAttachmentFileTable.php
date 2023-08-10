1cad320e9af91ec1500dcba40c2a2ff062fcbe81
Hotlander <j.casteleijn@labelgoods.nl>
2023-08-10 09:42:45 +0200

    bugfix/MAR10001-0-add-listener-to-set-external-url: - Added listener to add additional external url for product images in api - Added setting to enable/disable the listener - Added tests for listeners

---
+++ src/Marello/Bundle/ProductBundle/Migrations/Schema/v1_14/UpdateAttachmentFileTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Marello\Bundle\ProductBundle\Migrations\Schema\v1_14;
+
+use Doctrine\DBAL\Schema\Schema;
+
+use Oro\Bundle\MigrationBundle\Migration\QueryBag;
+use Oro\Bundle\MigrationBundle\Migration\Migration;
+use Oro\Bundle\EntityExtendBundle\EntityConfig\ExtendScope;
+
+class UpdateAttachmentFileTable implements Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function up(Schema $schema, QueryBag $queries)
+    {
+        /** Add additional media url */
+        $this->addAdditionalMediaUrl($schema);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    protected function addAdditionalMediaUrl(Schema $schema)
+    {
+        $table = $schema->getTable('oro_attachment_file');
+        if (!$table->hasColumn('media_url')) {
+            $table->addColumn('media_url', 'string', [
+                'oro_options' => [
+                    'extend' => [
+                        'is_extend' => true,
+                        'owner' => ExtendScope::OWNER_CUSTOM,
+                        'nullable' => true,
+                        'on_delete' => 'SET NULL'
+                    ],
+                ],
+                [
+                    'length' => 255,
+                    'notnull' => false
+                ]
+            ]);
+        }
+    }
+}
