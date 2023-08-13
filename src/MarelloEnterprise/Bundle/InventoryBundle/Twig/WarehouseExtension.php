073849d3db0fe4aebca16de1aa49f8cde83aa1d4
Viacheslav Bemza <bemzaslava@gmail.com>
2023-08-13 13:30:50 +0300

    MAR10001-1167: Sell-by Date on an inventory Batch - moved from ee to ce version

--- src/MarelloEnterprise/Bundle/InventoryBundle/Twig/WarehouseExtension.php
+++ src/MarelloEnterprise/Bundle/InventoryBundle/Twig/WarehouseExtension.php
@@ -2,9 +2,7 @@
 
 namespace MarelloEnterprise\Bundle\InventoryBundle\Twig;
 
-use Marello\Bundle\InventoryBundle\Entity\InventoryItem;
 use MarelloEnterprise\Bundle\InventoryBundle\Checker\IsFixedWarehouseGroupChecker;
-use MarelloEnterprise\Bundle\InventoryBundle\Manager\InventoryManager;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -13,10 +11,8 @@ class WarehouseExtension extends AbstractExtension
     const NAME = 'marelloenterprise_warehouse';
 
     public function __construct(
-        protected IsFixedWarehouseGroupChecker $isFixedWarehouseGroupChecker,
-        protected InventoryManager $inventoryManager
-    ) {
-    }
+        protected IsFixedWarehouseGroupChecker $isFixedWarehouseGroupChecker
+    ) {}
 
     /**
      * {@inheritdoc}
@@ -36,24 +32,6 @@ class WarehouseExtension extends AbstractExtension
                 'marello_inventory_is_fixed_warehousegroup',
                 [$this->isFixedWarehouseGroupChecker, 'check']
             ),
-            new TwigFunction(
-                'get_expected_inventory_total',
-                [$this, 'getExpectedInventoryTotal']
-            ),
-            new TwigFunction(
-                'get_expired_sell_by_date_total',
-                [$this, 'getExpiredSellByDateTotal']
-            ),
         ];
-    }
-
-    public function getExpectedInventoryTotal($entity)
-    {
-        return $this->inventoryManager->getExpectedInventoryTotal($entity);
-    }
-
-    public function getExpiredSellByDateTotal(InventoryItem $entity)
-    {
-        return $this->inventoryManager->getExpiredSellByDateTotal($entity);
     }
 }
